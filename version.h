1382bf0e9805e1d2d3c6613f079543a99274eb7d
WileyECoyote <wileyhill@gmail.com>
2014-07-19 23:19:08 -0500

    Don't ignore version.h

---
+++ version.h
@@ -0,0 +1,11 @@
+/* Date part of package version string. */
+#define PACKAGE_DATE_VERSION "20140719"
+
+/* Dotted part of package version string. */
+#define PACKAGE_DOTTED_VERSION "2.0.8.6"
+
+/* Current git commit. */
+#define PACKAGE_GIT_COMMIT "63dd8a577bdc58dbbf851d4a07b2d07bd91720a9"
+
+/* Current 'git describe' version string. */
+#define PACKAGE_GIT_VERSION "1.7.2-20111231-208-g63dd8a5"
