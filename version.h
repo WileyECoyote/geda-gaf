db5bb7beaa2a0250a022c76061dca19e7af8a616
WileyECoyote <wileyhill@gmail.com>
2014-07-22 23:47:28 -0500

    Redo INPUT OUPUT symbols, + new INA193 symbols

--- version.h
+++ version.h
@@ -5,7 +5,7 @@
 #define PACKAGE_DOTTED_VERSION "2.0.8.7"
 
 /* Current git commit. */
-#define PACKAGE_GIT_COMMIT "0d17020027a6d67ab6ea852c5fb3ea7e7e1ed9be"
+#define PACKAGE_GIT_COMMIT "bea94e2aed164e4331ed1a025e1db45149d1ca0c"
 
 /* Current 'git describe' version string. */
-#define PACKAGE_GIT_VERSION "1.7.2-20111231-222-g0d17020"
+#define PACKAGE_GIT_VERSION "1.7.2-20111231-224-gbea94e2"
