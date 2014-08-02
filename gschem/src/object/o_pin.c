8c6fc722006bfc91445c22c5f7e80e03acb81f02
WileyECoyote <wileyhill@gmail.com>
2014-08-02 00:22:37 -0500

    Remove set page changed in o_pin_end

--- gschem/src/object/o_pin.c
+++ gschem/src/object/o_pin.c
@@ -75,10 +75,8 @@ void o_pin_end(GschemToplevel *w_current, int x, int y)
   s_page_append_object (toplevel->page_current, new_obj);
 
   /* Call add-objects-hook */
-
   g_run_hook_object (w_current, "%add-objects-hook", new_obj);
 
-  toplevel->page_current->CHANGED=1;
   o_undo_savestate(w_current, UNDO_ALL);
 }
 
