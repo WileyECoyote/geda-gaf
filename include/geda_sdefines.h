b991f5b0fb7b2fa915cb670679df0fb693f96acd
WileyECoyote <wileyhill@gmail.com>
2014-08-07 20:57:45 -0500

    Missing file include/geda_sdefines.h

---
+++ include/geda_sdefines.h
@@ -0,0 +1,34 @@
+/* C header                                           -*- geda_sdefines.h -*-
+ * file: geda_sdefines.h
+ *
+ * gEDA - GPL Electronic Design Automation
+ *
+ * Copyright (C) 2013-2014 Wiley Edward Hill
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA
+ */
+
+/* W.E.Hill Oct 16, 2013 relocated "these" defines from gschem, and
+ * on August 05, 2015 from libgeda, so all gaf's can reference.
+ */
+#define SCHEMATIC_FILE_SUFFIX      "sch"
+#define SCHEMATIC_FILE_DOT_SUFFIX  ".sch"
+#define SCHEMATIC_FILTER           "*.sch"
+
+#define SYMBOL_FILE_SUFFIX         "sym"
+#define SYMBOL_FILE_DOT_SUFFIX     ".sym"
+#define SYMBOL_FILTER              "*.sym"
+
