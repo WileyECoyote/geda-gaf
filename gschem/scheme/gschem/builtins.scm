c7846cdfaa342f3aa14936496b62fd284836f95d
WileyECoyote <wileyhill@gmail.com>
2014-08-09 02:20:52 -0500

    Update copyrights

--- gschem/scheme/gschem/builtins.scm
+++ gschem/scheme/gschem/builtins.scm
@@ -1,6 +1,6 @@
 ;; gEDA - GPL Electronic Design Automation
 ;; gschem - gEDA Schematic Capture - Scheme API
-;; Copyright (C) 2013 Peter Brett <peter@peter-b.co.uk>
+;; Copyright (C) 2013-2014 Peter Brett <peter@peter-b.co.uk>
 ;;
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
