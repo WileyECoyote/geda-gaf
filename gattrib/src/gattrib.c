e39e92bac3a25d8ccf5b96fd2d0f1e0764ddc207
WileyECoyote <wileyhill@gmail.com>
2014-10-29 06:32:32 -0500

    Reduce initial log message string, was too long and causing faults

--- gattrib/src/gattrib.c
+++ gattrib/src/gattrib.c
@@ -282,11 +282,9 @@ void gattrib_main(void *closure, int argc, char *argv[])
 
   /* ----------  create log file right away ---------- */
   u_log_init ("gattrib");
-
-  u_log_message
-    (_("gEDA/gattrib version %s%s.%s\ngEDA/gattrib comes with ABSOLUTELY NO WARRANTY; see COPYING for more details.\nThis is free software, and you are welcome to redistribute it under certain\nconditions; please see the COPYING file for more details.\n\n"),
-     PREPEND_VERSION_STRING, PACKAGE_DOTTED_VERSION,
-     PACKAGE_DATE_VERSION);
+  u_log_message("gEDA/gattrib version %s%s.%s\n", PREPEND_VERSION_STRING,
+                                                  PACKAGE_DOTTED_VERSION,
+                                                  PACKAGE_DATE_VERSION);
 
   /* register guile (scheme) functions, this is necessary to parse RC file */
   g_register_funcs();
