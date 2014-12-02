4fae5c592b70cfdcc4c1d0fa41c6c17456431b84
WileyECoyote <wileyhill@gmail.com>
2014-12-02 07:51:54 -0600

    Continuing Experiments with X11

--- libgedadraw/src/x11/geda-x11.c
+++ libgedadraw/src/x11/geda-x11.c
@@ -37,9 +37,9 @@ int geda_x11_draw_line (GedaDrawData *draw_data, int x1, int y1, int x2, int y2)
   XGCValues gcvals;
 
   gcvals.line_width = LINE_WIDTH(object->line_options->line_width);
-  XChangeGC(draw_data->display, draw_data->gc, GCLineWidth, &gcvals);
+  XChangeGC(display, draw_data->gc, GCLineWidth, &gcvals);
   XSetForeground(display, draw_data->gc, buildColor(draw_data)); 
-//fprintf (stderr, "from (%d,%d) to (%d,%d)\n", x1, y1, x2, y2);
+  fprintf (stderr, "from (%d,%d) to (%d,%d, width=%d)\n", x1, y1, x2, y2, gcvals.line_width);
   XDrawLine(draw_data->display, draw_data->drawable, draw_data->gc, x1, y1, x2, y2);
   return 0;
 }
