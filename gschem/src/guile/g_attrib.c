e8d694ff6b0e87594e014d42742242f7e034be74
WileyECoyote <wileyhill@gmail.com>
2015-01-10 18:24:33 -0600

    Update file header

--- gschem/src/guile/g_attrib.c
+++ gschem/src/guile/g_attrib.c
@@ -1,4 +1,8 @@
-/* gEDA - GPL Electronic Design Automation
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4 tab-width: 4 -*- */
+/*
+ * File: g_attrib.c
+ *
+ * gEDA - GPL Electronic Design Automation
  * gschem - gEDA Schematic Capture
  *
  * Copyright (C) 2011-2015 Peter Brett <peter@peter-b.co.uk>
@@ -15,10 +19,9 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301 USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA <http://www.gnu.org/licenses/>.
  */
-
 /*!
  * \file g_attrib.c
  * \brief Scheme API functions for manipulating attributes in
