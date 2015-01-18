c2926ab864e545c4db11a4799d9c09b67d64467f
WileyECoyote <wileyhill@gmail.com>
2015-01-17 21:04:22 -0600

    Just multiple, eliminate pow function

--- libgeda/src/math/m_basic.c
+++ libgeda/src/math/m_basic.c
@@ -40,7 +40,7 @@
  */
 double m_distance(int x1, int y1, int x2, int y2)
 {
-  return sqrt(pow(x1-x2,2)+pow(y1-y2,2));
+  return sqrt((x1-x2) * (x1-x2) + (y1-y2) * (y1-y2));
 }
 
 /*! \brief Convert Paper size to World coordinates.
