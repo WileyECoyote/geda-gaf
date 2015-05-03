3f460bd05cce86ac8a2df816b4f9af1d650e0fca
WileyECoyote <wileyhill@gmail.com>
2015-05-02 23:36:02 -0500

    Modify ToolbarStringData structure

--- include/geda_toolbars.h
+++ include/geda_toolbars.h
@@ -19,26 +19,41 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301 USA
  */
+/* ------------------------------------------------------------------
+ * WEH | 05/02/15 | Modify ToolbarStringData structure; remove members
+ *                | Widget and Private, add member icon (to hold a pointer
+ *                | to the image widget for the butoon.)
+*/
 
 /* ------------------------------------------------------------------ */
 
 #ifndef __GEDA__TOOLBARS__
 #define __GEDA__TOOLBARS__
 
-
+/*! \struct  ToolbarStringData String data record for creating toolbar items
+ *  \par
+ *  action:
+ *           1. Must be unadulterated action string
+ *              a. string is loaded into widget as "action" data, which is later
+ *                 retrieved by func (x_toolbars_execute) when the button is clicked.
+ *              b. Primary icon is determined from the action string
+ *
+ */
 typedef struct
 {
-   const char *Widget;
+   const char *action;
    const char *Label;
    const char *Tip;
-   const char *Private;
+   const char *icon_id; /* Default and Backup Icon name */
+   int         iflag;   /* Flag mask use to describe the icon source */
+   GtkWidget  *icon;
 } ToolbarStringData;
 
 typedef struct
 {
    char *Widget;
-   int  ButtonId;
-   int  ToolBarId;
+   int   ButtonId;
+   int   ToolBarId;
    char *stock_id;
 
 } ToolbarItem;
@@ -49,11 +64,13 @@ typedef struct
 
 } ToolbarConfig;
 
-#define TB_WIDGET(member)    ToolbarStrings[member].Widget
-#define TB_LABEL(member)     ToolbarStrings[member].Label
-#define TB_TOOLTIP(member)   ToolbarStrings[member].Tip
-#define TB_PRIVATE(member)   ToolbarStrings[member].Private
-#define TB_ICON_NAME(member) ToolbarStrings[member].Private
+#define TB_SMALL_ICON GTK_ICON_SIZE_SMALL_TOOLBAR
+
+#define TB_ACTION(member)     ToolbarStrings[member].action
+#define TB_LABEL(member)      ToolbarStrings[member].Label
+#define TB_TOOLTIP(member)    ToolbarStrings[member].Tip
+#define TB_ICON_NAME(member)  ToolbarStrings[member].icon_id
+#define TB_ICON_IMAGE(member) ToolbarStrings[member].icon
 
 #define TB_BUTTON(member) member##_button
 
@@ -101,14 +118,14 @@ typedef struct
 /*! \brief 1st LeveL Intermediate ToolBar Macro with Enumerated data */
 #define TOOLBAR_BUTTON( bar, name, type, icon, button, func, data) { \
         TOOLBAR_BUTTON_##type (bar, name, ENUM, icon, button, func, data) \
-        GEDA_TOOLBAR_BUTTON_ATK(bar, button, TB_TOOLTIP (name), TB_WIDGET(name)) \
+        GEDA_TOOLBAR_BUTTON_ATK(bar, button, TB_TOOLTIP (name), TB_ACTION(name)) \
         g_object_set (button, "visible", TRUE, NULL); \
 }
 
 /*! \brief 1st LeveL Intermediate ToolBar Macro with *w_current data */
 #define TOOLBAR_BUTTON_DATA( bar, name, type, icon, button, func, data) { \
         TOOLBAR_BUTTON_##type (bar, name, GEDA, icon, button, func, data) \
-        GEDA_TOOLBAR_BUTTON_ATK(bar, button, TB_TOOLTIP (name), TB_WIDGET(name)) \
+        GEDA_TOOLBAR_BUTTON_ATK(bar, button, TB_TOOLTIP (name), TB_ACTION(name)) \
         g_object_set (button, "visible", TRUE, NULL); \
 }
 
@@ -116,43 +133,43 @@ typedef struct
 /*! \brief 2nd Level Intermediate ToolBar Macro with Pixmap Icon */
 #define TOOLBAR_BUTTON_PIX(bar, name, next, icon, button, func, data) \
    tmp_toolbar_icon = create_pixmap (icon); /* from file */ \
-   next##_TOOLBAR_BUTTON(bar, tmp_toolbar_icon, button, _(TB_LABEL (name)), _(TB_TOOLTIP (name)), _(TB_PRIVATE (name)), func, data) \
-   g_object_set_data ((GObject*) button, "action", (void*)TB_WIDGET(name));
+   next##_TOOLBAR_BUTTON(bar, tmp_toolbar_icon, button, _(TB_LABEL (name)), _(TB_TOOLTIP (name)), _(TB_ICON_NAME (name)), func, data) \
+   g_object_set_data ((GObject*) button, "action", (void*)TB_ACTION(name));
 
 /*! \brief 2nd Level Intermediate ToolBar Macro with Stock Icon */
 #define TOOLBAR_BUTTON_STK(bar, name, next, icon, button, func, data) \
-   tmp_toolbar_icon = gtk_image_new_from_stock(STOCK_MAP(icon), GTK_ICON_SIZE_SMALL_TOOLBAR); \
-   next##_TOOLBAR_BUTTON(bar, tmp_toolbar_icon, button, _(TB_LABEL (name)), _(TB_TOOLTIP (name)), _(TB_PRIVATE (name)), func, data) \
-   g_object_set_data ((GObject*) button, "action", (void*)TB_WIDGET(name));
+   tmp_toolbar_icon = gtk_image_new_from_stock(STOCK_MAP(icon), TB_SMALL_ICON); \
+   next##_TOOLBAR_BUTTON(bar, tmp_toolbar_icon, button, _(TB_LABEL (name)), _(TB_TOOLTIP (name)), _(TB_ICON_NAME (name)), func, data) \
+   g_object_set_data ((GObject*) button, "action", (void*)TB_ACTION(name));
 
 /*! \brief 2nd Level Intermediate ToolBar Macro with Local Pixmap Icon */
 #define TOOLBAR_BUTTON_LOCAL_PIX(bar, name, next, icon, button, func, data) \
    GtkWidget *name##_ICN = create_pixmap (icon); /* from file */ \
-   next##_TOOLBAR_BUTTON(bar, name##_ICN, button, _(TB_LABEL (name)), _(TB_TOOLTIP (name)), _(TB_PRIVATE (name)), func, data) \
-   g_object_set_data ((GObject*) button, "action", (void*)TB_WIDGET(name));
+   next##_TOOLBAR_BUTTON(bar, name##_ICN, button, _(TB_LABEL (name)), _(TB_TOOLTIP (name)), _(TB_ICON_NAME (name)), func, data) \
+   g_object_set_data ((GObject*) button, "action", (void*)TB_ACTION(name));
 
 #define TOOLBAR_BUTTON_LOCAL_STR(bar, name, next, icon, button, func, data) \
    GtkWidget *name##_ICN = create_pixmap (TB_ICON_NAME(name)); /* from file */ \
-   next##_TOOLBAR_BUTTON(bar, name##_ICN, button, _(TB_LABEL (name)), _(TB_TOOLTIP (name)), _(TB_PRIVATE (name)), func, data) \
-   g_object_set_data ((GObject*) button, "action", (void*)TB_WIDGET(name));
+   next##_TOOLBAR_BUTTON(bar, name##_ICN, button, _(TB_LABEL (name)), _(TB_TOOLTIP (name)), _(TB_ICON_NAME (name)), func, data) \
+   g_object_set_data ((GObject*) button, "action", (void*)TB_ACTION(name));
 
 /*! \brief 2nd Level Intermediate ToolBar Macro with Local Factory Icon */
 #define TOOLBAR_BUTTON_LOCAL_FAC(bar, name, next, icon, button, func, data) \
-   GtkWidget *name##_ICN = gtk_image_new_from_stock(TB_ICON_NAME(name), GTK_ICON_SIZE_SMALL_TOOLBAR); \
-   next##_TOOLBAR_BUTTON(bar, name##_ICN, button, _(TB_LABEL (name)), _(TB_TOOLTIP (name)), _(TB_PRIVATE (name)), func, data) \
-   g_object_set_data ((GObject*) button, "action", (void*)TB_WIDGET(name));
+   GtkWidget *name##_ICN = gtk_image_new_from_stock(TB_ICON_NAME(name), TB_SMALL_ICON); \
+   next##_TOOLBAR_BUTTON(bar, name##_ICN, button, _(TB_LABEL (name)), _(TB_TOOLTIP (name)), _(TB_ICON_NAME (name)), func, data) \
+   g_object_set_data ((GObject*) button, "action", (void*)TB_ACTION(name));
 
 /*! \brief 2nd Level Intermediate ToolBar Macro with Action Theme Icon */
 #define TOOLBAR_BUTTON_THEME(bar, name, next, icon, button, func, data) \
    GtkWidget *name##_ICN = gtk_image_new_from_icon_name(TB_ICON_NAME(name), GTK_ICON_SIZE_BUTTON); \
-   next##_TOOLBAR_BUTTON(bar, name##_ICN, button, _(TB_LABEL (name)), _(TB_TOOLTIP (name)), _(TB_PRIVATE (name)), func, data) \
-   g_object_set_data ((GObject*) button, "action", (void*)TB_WIDGET(name));
+   next##_TOOLBAR_BUTTON(bar, name##_ICN, button, _(TB_LABEL (name)), _(TB_TOOLTIP (name)), _(TB_ICON_NAME (name)), func, data) \
+   g_object_set_data ((GObject*) button, "action", (void*)TB_ACTION(name));
 
 /*! \brief 2nd Level Intermediate ToolBar Macro with Local Stock Icon */
 #define TOOLBAR_BUTTON_LOCAL_STK(bar, name, next, icon, button, func, data) \
-   GtkWidget *name##_ICN = gtk_image_new_from_stock(STOCK_MAP(icon), GTK_ICON_SIZE_SMALL_TOOLBAR); \
-   next##_TOOLBAR_BUTTON(bar, name##_ICN, button, _(TB_LABEL (name)), _(TB_TOOLTIP (name)), _(TB_PRIVATE (name)), func, data) \
-   g_object_set_data ((GObject*) button, "action", (void*)TB_WIDGET(name));
+   GtkWidget *name##_ICN = gtk_image_new_from_stock(STOCK_MAP(icon), TB_SMALL_ICON); \
+   next##_TOOLBAR_BUTTON(bar, name##_ICN, button, _(TB_LABEL (name)), _(TB_TOOLTIP (name)), _(TB_ICON_NAME (name)), func, data) \
+   g_object_set_data ((GObject*) button, "action", (void*)TB_ACTION(name));
 
 /*! \brief 2nd Level Intermediate ToolBar Macro with Item Data Struct */
 #define TOOLBAR_BUTTON_LOCAL_ALT(bar, name, next, icon, button, func, data) \
@@ -161,7 +178,7 @@ typedef struct
    name##_ToolbarItem.ButtonId =  name; \
    GtkWidget *name##_ICN = get_stock_alt_pixmap (w_current, &name##_ToolbarItem); \
    next##_TOOLBAR_BUTTON(bar, name##_ICN, button, _(TB_LABEL (name)), _(TB_TOOLTIP (name)), _(TB_TOOLTIP (name)), func, data) \
-   g_object_set_data ((GObject*) button, "action", (void*)TB_WIDGET(name));
+   g_object_set_data ((GObject*) button, "action", (void*)TB_ACTION(name));
 
 /* --------------------------- 3rd Level -------------------------------- */
 #define ENUM_TOOLBAR_BUTTON(bar, icon, button, txt, tip, priv, func, data) \
@@ -190,7 +207,7 @@ typedef struct
 #endif
 
 #define TOOLBAR_GSCHEM_RADIO_STK( bar, var, grp, name, icon, func, data) \
-   GtkWidget *name##_ICN = gtk_image_new_from_stock(icon, GTK_ICON_SIZE_SMALL_TOOLBAR); \
+   GtkWidget *name##_ICN = gtk_image_new_from_stock(icon, TB_SMALL_ICON); \
    TOOLBAR_GSCHEM_RADIO_ELEMENT ( bar##_Toolbar, name##_ICN, var, grp, name, func, data)
 
 #define TOOLBAR_GSCHEM_RADIO_ELEMENT( bar, icon, var, grp, name, func, data) \
@@ -199,14 +216,14 @@ typedef struct
                                     grp, \
                                    _(TB_LABEL (name)), \
                                    _(TB_TOOLTIP (name)), \
-                                     TB_WIDGET(name), \
+                                     TB_ACTION(name), \
                                      GTK_WIDGET(icon), \
                                     (GtkSignalFunc) func, \
                                      data); \
-   g_object_set_data ((GObject*) var, "action", (void*)TB_WIDGET(name)); \
+   g_object_set_data ((GObject*) var, "action", (void*)TB_ACTION(name)); \
    { \
      AtkObject *atk_obj = gtk_widget_get_accessible(var); \
-     char *str = u_string_concat("Geda-toolbar-button-", TB_WIDGET(name), "-mode", NULL); \
+     char *str = u_string_concat("Geda-toolbar-button-", TB_ACTION(name), "-mode", NULL); \
      gtk_widget_set_name (var, str); \
            str = g_strdelimit(str, "-", ' ' ); \
      atk_object_set_name (atk_obj, _(str)); \
