fc35dc93ec618f440356889ea2d0cbfcf7cc5d66
WileyECoyote <wileyhill@gmail.com>
2015-06-26 20:07:29 -0500

    Add header file gschem types

---
+++ gschem/include/gschem_types.h
@@ -0,0 +1,39 @@
+/* -*- gschem_types.h -*-
+ *
+ * gEDA - GPL Electronic Design Automation
+ * gschem - gEDA Schematic Capture
+ *
+ * Copyright (C) 2015 Wiley Edward Hill
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Boston, MA 02110-1301 USA
+ *
+ *  Contributing Author: Wiley Edward Hill
+ *  Date Contributed: June, 21, 2015
+ *
+ */
+/*! \file gschem_types.h
+ *
+ *  \brief List all Builtin User Interface Commands/Actions
+ *  \par Description
+ *   Maybe a temporary scheme used to synchronize the action
+ *
+ */
+#ifndef _GSCHEM_TYPES_H
+#define _GSCHEM_TYPES_H
+
+/*! \brief Type of callback function for event handler */
+typedef void (*EventResolver)(GschemToplevel*, int, int);
+
+#endif
