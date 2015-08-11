d6fd1cbc93d402a8b1c8fadd2601f96fd6d98e36
WileyECoyote <wileyhill@gmail.com>
2015-08-11 00:02:19 -0500

    Libgeda/tests: Move setting up and reset  of component library inside function

--- libgeda/scheme/unit-tests/t0113-object-library.scm
+++ libgeda/scheme/unit-tests/t0113-object-library.scm
@@ -4,16 +4,16 @@
 (use-modules (geda object))
 (use-modules (geda page))
 
-;; Set up component library, making blatant assumptions about the
-;; directory layout. WEH 04/03/14: Hacked to get around distchecks
-(let ((norm (string-join (list (getenv "srcdir") "../../symbols/passive/resistor") "/"))
+(begin-test 'component/library
+  ;; Set up component library, making blatant assumptions about the
+  ;; directory layout. WEH 04/03/14: Hacked to get around distchecks
+  (let ((norm (string-join (list (getenv "srcdir") "../../symbols/passive/resistor") "/"))
       (dist (string-join (list (getenv "srcdir") "../../../symbols/passive/resistor") "/"))
      )
        (if (access? dist R_OK)
            (component-library dist "Basic devices")
            (component-library norm "Basic devices")))
 
-(begin-test 'component/library
   (let ( (A (make-component/library "resistor-1.sym" '(1 . 2) 0 #t #f))
          (B (make-component/library "invalid-component-name" '(1 . 2) 0 #t #f)))
 
@@ -28,9 +28,7 @@
     (assert-true (not (null? (component-contents A))))
 
     (assert-true (not B)))
+
+  ;; Clear component library again
+  (reset-component-library)
 )
-
-;; Clear component library again
-(reset-component-library)
-
-
