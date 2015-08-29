343586690188babbe922a80db1db9fd7331ee7dc
WileyECoyote <wileyhill@gmail.com>
2015-08-29 05:35:01 -0500

    libgeda/scheme: Update file headers: FSF address

--- libgeda/scheme/geda.scm
+++ libgeda/scheme/geda.scm
@@ -1,4 +1,26 @@
-; -*-Scheme-*-
+;; -*-Scheme-*-
+;; gEDA - GPL Electronic Design Automation
+;; libgeda - gEDA's library - Scheme API
+;;
+;; Copyright (C) 2010-2015 Peter Brett <peter@peter-b.co.uk>
+;; Copyright (C) 2011-2015 gEDA Contributors (see ChangeLog for details)
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+;; MA 02110-1301 USA
+;;
+
 (use-modules (geda os) (ice-9 optargs) (ice-9 ftw))
 (define path-sep separator)
 (define geda-data-path (car (sys-data-dirs)))
@@ -45,7 +67,7 @@
           (if (and (regular-file? path)
                    (has-suffix? path ".scm")
                    (access? path R_OK))
-            (eval-protected `(load ,path))
+            (eval-protected `(primitive-load ,path))
             #f
           )))
       (closedir dir))
--- libgeda/scheme/geda/attrib.scm
+++ libgeda/scheme/geda/attrib.scm
@@ -1,6 +1,10 @@
+;; -*-Scheme-*-
+;;
 ;; gEDA - GPL Electronic Design Automation
 ;; libgeda - gEDA's library - Scheme API
-;; Copyright (C) 2010-2011 Peter Brett <peter@peter-b.co.uk>
+;;
+;; Copyright (C) 2010-2015 Peter Brett <peter@peter-b.co.uk>
+;; Copyright (C) 2011-2015 gEDA Contributors (see ChangeLog for details)
 ;;
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -14,7 +18,8 @@
 ;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program; if not, write to the Free Software
-;; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+;; MA 02110-1301 USA
 ;;
 
 (define-module (geda attrib)
