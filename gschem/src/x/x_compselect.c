530a7556a18e0a50167084d6a36ec6c615923610
WileyECoyote <wileyhill@gmail.com>
2015-09-25 10:53:40 -0500

    Remove GTK_WIDGET_IS_SENSITIVE

--- gschem/src/x/x_compselect.c
+++ gschem/src/x/x_compselect.c
@@ -995,7 +995,7 @@ compselect_callback_filter_entry_changed (GtkEditable *editable,
 
   sensitive = (g_ascii_strcasecmp (text,"") != 0);
 
-  if (GTK_WIDGET_IS_SENSITIVE (button) != sensitive) {
+  if (gtk_widget_is_sensitive (button) != sensitive) {
     gtk_widget_set_sensitive (button, sensitive);
   }
 
