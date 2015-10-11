f381968e6975dbb20862f38a6dbae9cda812a9ee
WileyECoyote <wileyhill@gmail.com>
2015-10-11 13:34:41 -0500

    Libgeda.scheme: Update copyright information

--- libgeda/src/scheme/scheme_config.c
+++ libgeda/src/scheme/scheme_config.c
@@ -1,6 +1,6 @@
 /* gEDA - GPL Electronic Design Automation
  * libgeda - gEDA's library - Scheme API
- * Copyright (C) 2011-2014 Peter Brett <peter@peter-b.co.uk>
+ * Copyright (C) 2011-2015 Peter Brett <peter@peter-b.co.uk>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
