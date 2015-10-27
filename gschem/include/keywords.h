e565484892eab269788aeff6268b485a824541c1
WileyECoyote <wileyhill@gmail.com>
2015-10-26 23:49:54 -0500

    Replace header comments

--- gschem/include/keywords.h
+++ gschem/include/keywords.h
@@ -1,32 +1,32 @@
 /* C header
-;  File: keywords.h
-;;
-;;; gEDA - GPL Electronic Design Automation
-;;; gschem - gEDA Schematic Capture
-;;; Copyright (C) 1998-2015 Ales Hvezda
-;;; Copyright (C) 1998-2015 gEDA Contributors (see ChangeLog for details)
-;;
-;;; Copyright (C) 2012-2013 Wiley Edward Hill <wileyhill@gmail.com>
-;;
-;;; This program is free software; you can redistribute it and/or modify
-;;; it under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation; either version 2 of the License, or
-;;; (at your option) any later version.
-;;;
-;;; This program is distributed in the hope that it will be useful,
-;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;; GNU General Public License for more details.
-;;;
-;;; You should have received a copy of the GNU General Public License
-;;; along with this program; if not, write to the Free Software
-;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-;;; MA 02110-1301 USA
-;;
-;;; Date: Aug, 17, 2012
-;;; Contributing Author: Wiley Edward Hill
-;;
-*/
+ * File: keywords.h
+ *
+ * gEDA - GPL Electronic Design Automation
+ * gschem - gEDA Schematic Capture
+ *
+ * Copyright (C) 1998-2015 Ales Hvezda
+ * Copyright (C) 2012-2015 Wiley Edward Hill <wileyhill@gmail.com>
+ * Copyright (C) 1998-2015 gEDA Contributors (see ChangeLog for details)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA, <http://www.gnu.org/licenses/>.
+ *
+ * Date: Aug, 17, 2012
+ * Contributing Author: Wiley Edward Hill
+ */
+
 /************************ REVISION HISTORY *************************
 ;; Who |   When   |  What (Why)
 ;; ------------------------------------------------------------------
