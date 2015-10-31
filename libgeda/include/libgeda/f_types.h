2abce670d3872b72ac31dae8ae5f2d0d160b37d4
WileyECoyote <wileyhill@gmail.com>
2015-10-30 22:55:56 -0500

    libgeda: Add doxygen tags to function type definitions

--- libgeda/include/libgeda/f_types.h
+++ libgeda/include/libgeda/f_types.h
@@ -1,35 +1,47 @@
-/*! \brief Type of callback function  used by u_log_set_update_func */
+/*! \typedef LogUpdateFunc
+ *  \brief Type of callback function  used by u_log_set_update_func */
 typedef void (*LogUpdateFunc)(const char *, GLogLevelFlags,  const char *);
 
-/*! \brief Type of callback function used by g_rc_parse_handler */
+/*! \typedef ConfigParseErrorFunc
+ *  \brief Type of callback function used by g_rc_parse_handler */
 typedef void (*ConfigParseErrorFunc)(GError **, void *);
 
-/*! \brief Type of callback function for getting the bounds of a GedaObject */
+/*! \typedef ObjectBoundsFunc
+ *  \brief Type of callback function for getting the bounds of a GedaObject */
 typedef int (*ObjectBoundsFunc)(Object *);
 
-/*! \brief Type of callback function for calculating text bounds */
+/*! \typedef RenderedBoundsFunc
+ *  \brief Type of callback function for calculating text bounds */
 typedef int(*RenderedBoundsFunc)(void *, Object *, int *, int *, int *, int *);
 
-/*! \brief Type of callback function for object damage notification */
+/*! \typedef
+ *  \brief Type of callback function for object damage notification */
 typedef int(*ChangeNotifyFunc)(void *, Object *);
 
-/*! \brief Type of callback function for notification when a new Object is created */
+/*! \typedef NewObjectFunc
+ *  \brief Type of callback function for notification when a new Object is created */
 typedef void(*NewObjectFunc)(Object *, void *);
 
-/*! \brief Type of callback function for notification when a new GedaToplevel is created */
+/*! \typedef NewPageFunc
+ *  \brief Type of callback function for notification when a new GedaToplevel is created */
 typedef void(*NewPageFunc)(Page *, void *);
 
-/*! \brief Type of callback function for notification when a new GedaToplevel is created */
+/*! \typedef NewToplevelFunc
+ *  \brief Type of callback function for notification when a new GedaToplevel is created */
 typedef void(*NewToplevelFunc)(GedaToplevel *, void *);
 
-/*! \brief Type of callback function for notification when an object's attributes change */
+/*! \typedef AttribsChangedFunc
+ *  \brief Type of callback function for notification when an object's attributes change */
 typedef void(*AttribsChangedFunc)(void *, Object *);
 
-/*! \brief Type of callback function for notification when an object's connections change */
+/*! \typedef ConnsChangedFunc
+ *  \brief Type of callback function for notification when an object's connections change */
 typedef void(*ConnsChangedFunc)(void *, Object *);
 
-/*! \brief Type of callback function for querying loading of backups  */
+/*! \typedef LoadBackupQueryFunc
+ *  \brief Type of callback function for querying loading of backups  */
 typedef bool(*LoadBackupQueryFunc)( char *, void *);
 
-/*! \brief Type of callback function for object damage notification */
+/*! \typedef WeakNotifyFunc
+ *  \brief Type of callback function for object damage notification */
 typedef void (*WeakNotifyFunc)(void *, void *);
