b7c23107335e58f4613c313bcb7ce52d1000ae52
WileyECoyote <wileyhill@gmail.com>
2015-11-05 13:14:55 -0600

    libgeda: add GNU WEB address in file headers

--- libgeda/src/geda/geda_circle.c
+++ libgeda/src/geda/geda_circle.c
@@ -21,7 +21,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301 USA
+ * 02110-1301 USA, <http://www.gnu.org/licenses/>.
  *
  *  Contributing Author: Wiley Edward Hill
  *  Date Contributed: November, 18, 2013
