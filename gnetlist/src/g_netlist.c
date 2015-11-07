e20b9e8503b46e4fc83a78381a0e9037e0799f89
WileyECoyote <wileyhill@gmail.com>
2015-11-06 21:29:19 -0600

    gnetlist: explicitly release memory for c variables

--- gnetlist/src/g_netlist.c
+++ gnetlist/src/g_netlist.c
@@ -942,17 +942,14 @@ SCM g_get_input_files(void)
  */
 SCM g_graphical_objs_in_net_with_attrib_get_attrib (SCM scm_netname, SCM scm_has_attribute, SCM scm_wanted_attribute)
 {
-
   SCM list = SCM_EOL;
-  NETLIST *nl_current;
-  CPINLIST *pl_current;
-  char *wanted_net_name;
+  NETLIST  *nl_current;
+
   char *wanted_attrib;
   char *has_attrib;
   char *net_name;
-  char *attrib_value=NULL;
-  char *has_attrib_value = NULL;
-  char *has_attrib_name = NULL;
+
+  scm_dynwind_begin (0);
 
   SCM_ASSERT(scm_is_string (scm_netname), scm_netname, SCM_ARG1,
              "gnetlist:graphical-net-objs-attrib");
@@ -965,55 +962,57 @@ SCM g_graphical_objs_in_net_with_attrib_get_attrib (SCM scm_netname, SCM scm_has
              scm_has_attribute, SCM_ARG2,
              "gnetlist:graphical-objs-in-net-with-attrib-get-attrib");
 
-  scm_dynwind_begin (0);
-
-  wanted_net_name = scm_to_utf8_string (scm_netname);
-  if (wanted_net_name == NULL) {
-    return list;
-  }
-
-  scm_dynwind_free (wanted_net_name);
-
+  net_name      = scm_to_utf8_string (scm_netname);
   wanted_attrib = scm_to_utf8_string (scm_wanted_attribute);
-  scm_dynwind_free (wanted_attrib);
+  has_attrib    = scm_to_utf8_string (scm_has_attribute);
 
-  has_attrib = scm_to_utf8_string (scm_has_attribute);
-  scm_dynwind_free (has_attrib);
+  scm_dynwind_end ();
 
-  nl_current = graphical_netlist_head;
+  nl_current    = graphical_netlist_head;
 
-  /* walk through the list of components, and through the list
+  /* walk through list of components, and through the list
    * of individual pins on each, adding net names to the list
    * being careful to ignore duplicates, and unconnected pins
    */
   while (nl_current != NULL) {
-    pl_current = nl_current->cpins;
-    while (pl_current != NULL) {
-      if (pl_current->net_name) {
-        net_name = pl_current->net_name;
-        if (strcmp(net_name, wanted_net_name) == 0) {
 
-          if (o_attrib_string_get_name_value (has_attrib, &has_attrib_name,
-            &has_attrib_value) != 0) {
-            attrib_value =
+    CPINLIST *pl_current;
+
+    pl_current = nl_current->cpins;
+
+    while (pl_current != NULL) {
+
+      if (pl_current->net_name) {
+
+        char *current_name = pl_current->net_name;
+
+        if (strcmp(current_name, net_name) == 0) {
+
+          char *has_attrib_value = NULL;
+          char *has_attrib_name  = NULL;
+
+          if (o_attrib_string_get_name_value (has_attrib, &has_attrib_name, &has_attrib_value) != 0)
+          {
+            char *attrib_value =
             o_attrib_search_object_attribs_by_name (nl_current->object_ptr,
                                                     has_attrib_name, 0);
 
-            if ( ((has_attrib_value == NULL) && (attrib_value == NULL)) ||
-              ((has_attrib_value != NULL) && (attrib_value != NULL) &&
-              (strcmp(attrib_value, has_attrib_value) == 0)) ) {
+            if (((has_attrib_value == NULL) && (attrib_value == NULL)) ||
+                ((has_attrib_value != NULL) && (attrib_value != NULL) &&
+                 (strcmp(attrib_value, has_attrib_value) == 0)) )
+            {
               GEDA_FREE (attrib_value);
-            attrib_value =
-            o_attrib_search_object_attribs_by_name (nl_current->object_ptr,
-                                                    wanted_attrib, 0);
-            if (attrib_value) {
-              list = scm_cons (scm_from_utf8_string (attrib_value), list);
-            }
-            GEDA_FREE (attrib_value);
+              attrib_value =
+              o_attrib_search_object_attribs_by_name (nl_current->object_ptr,
+                                                      wanted_attrib, 0);
+              if (attrib_value) {
+                list = scm_cons (scm_from_utf8_string (attrib_value), list);
               }
-              GEDA_FREE (has_attrib_name);
-              GEDA_FREE (has_attrib_value);
+              GEDA_FREE (attrib_value);
             }
+            GEDA_FREE (has_attrib_name);
+            GEDA_FREE (has_attrib_value);
+          }
         }
       }
       pl_current = pl_current->next;
@@ -1021,7 +1020,10 @@ SCM g_graphical_objs_in_net_with_attrib_get_attrib (SCM scm_netname, SCM scm_has
     nl_current = nl_current->next;
   }
 
-  scm_dynwind_end ();
+  free (has_attrib);
+  free (wanted_attrib);
+  free (net_name);
+
   return list;
 }
 
