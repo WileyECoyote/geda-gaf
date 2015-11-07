001b9db783092677fdf813d6980ac6c8f02169e6
WileyECoyote <wileyhill@gmail.com>
2015-11-06 21:04:42 -0600

    gTAG: Reorganized and update example

--- examples/gTAG/crdist.sh
+++ examples/gTAG/crdist.sh
@@ -3,10 +3,20 @@
 DATETAG=`date '+%Y%m%d'`
 DISTFILE=gTAG-sch-${DATETAG}
 
-rm -f gTAG-sch-*.tar.gz gTAG.bom gTAG.pcb
-gnetlist -g bom -o gTAG.bom gTAG.sch
+gTAG_Files="gTAG/attribs gTAG/crdist.sh gTAG/gafrc gTAG/gnetlistrc gTAG/gTAG.sch
+            gTAG/gTAG-pcb.net gTAG/ChangeLog-1.0 gTAG/README gTAG/bom/gTAG.bom
+            gTAG/bom/gTAG_bom.csv
+            gTAG/ps/*.ps
+            gTAG/sch/*.sch
+            gTAG/sht/*.sym
+            gTAG/sym/*.sym"
+
+rm -f gTAG-sch-*.tar.gz rm bom/gTAG.bom bom/gTAG_bom.csv *~ */*~ #gTAG.pcb
+gnetlist -g bom -o bom/gTAG.bom gTAG.sch
+gnetlist -g bom2 -o bom/gTAG_bom.csv gTAG.sch
 gnetlist -g PCB -o gTAG-pcb.net gTAG.sch
+
 cd ..
-tar cf gTAG/${DISTFILE}.tar gTAG/*.sym gTAG/*.sch gTAG/*rc gTAG/*.ps gTAG/gTAG.bom gTAG/gTAG-pcb.net  gTAG/README gTAG/COPYING gTAG/ChangeLog gTAG/crdist.sh
+tar cf gTAG/${DISTFILE}.tar $gTAG_Files
 cd gTAG
 gzip ${DISTFILE}.tar
