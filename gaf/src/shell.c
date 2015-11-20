1421dc9ba70380a53fc5952e702b014f5db25e09
WileyECoyote <wileyhill@gmail.com>
2015-11-20 02:03:06 -0600

    Make gaf shell work like geda-shell

--- gaf/src/shell.c
+++ gaf/src/shell.c
@@ -35,15 +35,16 @@
 #include <libgeda/libgeda.h>
 #include <libgeda/libgedaguile.h>
 
-#define shell_short_options "s:c:L:l:h"
+#define shell_short_options "e:hl:p:s:"
 
 static struct option shell_long_options[] =
   {
+    {"eval",   1, NULL, 'e'},
     {"help",   0, NULL, 'h'},
-    {"help", 0, NULL, 'h'},
-    {NULL, 0, NULL, 0},
     {"load",   1, NULL, 'l'},
+    {"path",   1, NULL, 'p'},
     {"scheme", 1, NULL, 's'},
+    {NULL,     0, NULL, 0},
   };
 
 static void
@@ -53,16 +54,16 @@ shell_usage (void)
 "\n"
 "Shell for interactive processing of gEDA data using Scheme.\n"
 "\n"
-"  -c EXPR        evaluate Scheme expression EXPR, and exit\n"
+"  -e, --eval <EXPR>   evaluate Scheme expression EXPR, and exit\n"
 "  -h, --help          display usage information and exit\n"
 "  -l, --load <FILE>   load Scheme source code from FILE\n"
+"  -p, --path <DIR>    add DIR to the front of the Guile load path\n"
 "  -s, --scheme <FILE> load Scheme source code from FILE, and exit\n"
 "  --                  stop scanning arguments; run interactively\n"
 "\n"
-"The above switches stop argument processing, and pass all\n"
+"The switch -e and -s stop argument processing, and pass all\n"
 "remaining arguments as the value of (command-line).\n"
 "\n"
-"  -L DIRECTORY   add DIRECTORY to the front of the Scheme load path\n"
 "\n"
 "Please report bugs to %s.\n"),
 PACKAGE_BUGREPORT);
@@ -70,47 +71,85 @@ PACKAGE_BUGREPORT);
 }
 
 /* Some symbols we need */
-SCM_SYMBOL (sym_begin, "begin");
-SCM_SYMBOL (sym_use_modules, "use-modules");
-SCM_SYMBOL (sym_ice_9, "ice-9");
-SCM_SYMBOL (sym_readline, "readline");
 SCM_SYMBOL (sym_activate_readline, "activate-readline");
+SCM_SYMBOL (sym_begin,             "begin");
+SCM_SYMBOL (sym_cons,              "cons");
+SCM_SYMBOL (sym_eval_string,       "eval-string");
+SCM_SYMBOL (sym_ice_9,             "ice-9");
+SCM_SYMBOL (sym_load,              "load");
+SCM_SYMBOL (sym_load_path,         "%load-path");
+SCM_SYMBOL (sym_quit,              "quit");
+SCM_SYMBOL (sym_readline,          "readline");
+SCM_SYMBOL (sym_set_x,             "set!");
+SCM_SYMBOL (sym_top_repl,          "top-repl");
+SCM_SYMBOL (sym_use_modules,       "use-modules");
 
 static void
 cmd_shell_impl (void *data, int argc, char **argv)
 {
-  int c, interactive = 1;
-  GedaToplevel *toplevel;
 
-  #include "shell.x"
+  GedaToplevel *toplevel;
+  SCM path_lst = SCM_EOL; /* We reverse! this before using it. */
+  SCM run_lst = SCM_EOL;   /* We reverse! this before using it. */
+  SCM scm_status;
+  int status;
+  int c, interactive = 1;
+
+#include "shell.x"
 
   /* Parse command-line arguments */
   while ((c = getopt_long (argc, argv, shell_short_options,
-                           shell_long_options, NULL)) != -1) {
+                           shell_long_options, NULL)) != -1)
+  {
     switch (c) {
-    case 0:
-      /* This is a long-form-only flag option, and has already been
-       * dealt with by getopt_long(). */
-      break;
-    case 's':
-    case 'c':
-      interactive = 0;
-      /* Intentionally falls through */
-    case 'l':
-    case 'L':
-      /* Do nothing, scm_shell() will deal with these. */
-      break;
-    case 'h':
-      shell_usage ();
-      break;
-    case '?':
-      /* getopt_long already printed an error message */
-      fprintf (stderr, _("\nRun `gaf shell --help' for more information.\n"));
-      exit (1);
-      break;
-    default:
-      g_assert_not_reached ();
+      case 0:
+        /* This is a long-form-only flag option, and has already been
+         * dealt with by getopt_long(). */
+        break;
+      case 'h':
+        shell_usage ();
+        break;
+      case 'e':
+        interactive = 0;
+        run_lst = scm_cons (scm_list_2 (sym_eval_string,
+                                        scm_from_locale_string (optarg)),
+                            run_lst);
+        break;
+      case 's':
+        interactive = 0;
+      case 'l':
+        run_lst = scm_cons (scm_list_2 (sym_load,
+                                        scm_from_locale_string (optarg)),
+                            run_lst);
+        break;
+      case 'p':
+        /* Add argument to %load-path */
+        path_lst = scm_cons (scm_list_3 (sym_set_x,
+                                         sym_load_path,
+                                         scm_list_3 (sym_cons,
+                                                     scm_from_locale_string (optarg),
+                                                     sym_load_path)),
+                             path_lst);
+        continue;
+      case '?':
+        /* getopt_long already printed an error message */
+        fprintf (stderr, _("\nRun `gaf shell --help' for more information.\n"));
+        exit (1);
+        break;
+      default:
+        g_assert_not_reached ();
     }
+  }
+
+  /* Set program arguments visible from Guile */
+  scm_set_program_arguments (argc - optind, argv + optind, "gaf shell");
+
+  /* First run the setup list */
+  if (path_lst != SCM_EOL) {
+    /* Reverse lists */
+    path_lst = scm_reverse_x (path_lst, SCM_UNDEFINED);
+    path_lst = scm_cons (sym_begin, path_lst);
+    scm_eval_x (path_lst, scm_current_module ());
   }
 
   libgeda_init (argc, argv);
@@ -122,32 +161,50 @@ cmd_shell_impl (void *data, int argc, char **argv)
    * version message. */
   if (interactive) {
 
-    fprintf (stderr, "gEDA %s (g%.7s)\n", PACKAGE_DOTTED_VERSION, PACKAGE_GIT_COMMIT);
+    /* Print version applicable */
+    if (isatty (1) && isatty (0)) {
+      printf ("gEDA %s (g%.7s)\n", PACKAGE_DOTTED_VERSION, PACKAGE_GIT_COMMIT);
+    }
 
 #ifdef __MINGW32__
     fprintf (stderr, "readline is not yet supported for MinGW builds\n");
 #else
 
-    SCM expr = scm_list_3 (sym_begin,
-                           scm_list_2 (sym_use_modules,
-                                       scm_list_2 (sym_ice_9, sym_readline)),
-                           scm_list_1 (sym_activate_readline));
-
-    scm_eval_x (expr, scm_current_module ());
+    run_lst = scm_cons (scm_list_2 (sym_use_modules,
+                                    scm_list_2 (sym_ice_9, sym_readline)),
+                        run_lst);
+    run_lst = scm_cons (scm_list_1 (sym_activate_readline), run_lst);
+    run_lst = scm_cons (scm_list_1 (sym_top_repl), run_lst);
 
 #endif /* __MINGW32__ */
 
   }
+  else {
+    run_lst = scm_cons (scm_list_1 (sym_quit), run_lst);
+  }
+
+  /* Reverse run list */
+  run_lst = scm_reverse_x (run_lst, SCM_UNDEFINED);
 
   /* Now load rc files, if necessary */
   if (g_getenv ("GAF_INHIBIT_RCFILES") == NULL) {
     g_rc_parse ("gaf shell", NULL, NULL);
   }
+
   i_vars_libgeda_set (toplevel); /* Ugh */
 
-  scm_shell (argc, argv); /* Doesn't return */
+  /* Finally evaluate run list */
+  run_lst = scm_cons (sym_begin, run_lst);
+
+  scm_status = scm_eval_x (run_lst, scm_current_module ());
+
+  status = scm_exit_status (scm_status);
 
   scm_dynwind_end ();
+
+  scm_remember_upto_here_2 (path_lst, run_lst);
+
+  exit (status);
 }
 
 int
