4bc951c80570b834e01f3796eb706f6de2bda89c
WileyECoyote <wileyhill@gmail.com>
2015-12-01 14:13:50 -0600

    gschem: Major revisions to tests script

--- gschem/tests/runtest.sh
+++ gschem/tests/runtest.sh
@@ -8,7 +8,7 @@
 #           schematic files and reporting the results. The script can
 #           be invoked from the Makefile or from the command-line.
 
-VER=0.0.8
+VER=0.0.9
 
 ERR_FILE_NOT_FOUND=2
 ERR_BAD_ARGS=65
@@ -27,6 +27,8 @@ PASSCOUNT=0
 FAILCOUNT=0
 
 PROGRAM="gschem"
+TMPGEDADIR="gEDA"
+
 . ./TEST_FUNCS
 
 # Show command line usage
@@ -34,7 +36,180 @@ show_help (){
    echo Usage:   `basename $0` '[-h] || [[-r || --regen] -v || --verbose] (no dir) input'
 }
 
-do_setup_environment()
+01_setup_path2libraries()
+{
+   test -z "$LD_LIBRARY_PATH" && OLD_LD_PATH=$LD_LIBRARY_PATH
+
+   mkdir -m 0775 -p "libs"
+
+   LIBS=$(ls ../../libgeda*/src/.libs/*.so*)
+
+   for link2lib in ${LIBS[*]} ; do
+    if [ -L $link2lib ] ; then
+        cp ${link2lib} "libs/"
+    fi
+   done
+
+   export LD_LIBRARY_PATH=$PWD/libs:$LD_LIBRARY_PATH
+
+   vecho "LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
+}
+
+02_setup_geda_environment ()
+{
+  # Creates a local gEDA directory and subdirectories, copies all files
+  # in ../etc and libgeda/etc into this directory, creates links to the
+  # top-level BITMAPS, then creates a create scheme directory and copies
+  # all files from:
+  #      ../scheme
+  #      ../scheme/gschem     (including subdirectory: gschem)
+  #      ../../libgeda/scheme (including subdirectory: geda)
+
+  local CWDSAVE=$PWD
+
+  # create temporary gEDA directory and required subdirs
+  mkdir -m 0777 -p ${TMPGEDADIR}
+  rc=$?
+  if test $rc -ne 0 ; then
+     echo "Failed to create directory ${TMPGEDADIR} with appropriate permissions"
+     echo "mkdir returned $rc"
+     exit 1
+  fi
+
+  cd ${TMPGEDADIR}
+  export GEDADATARC=$PWD
+  export GEDADATA=$PWD
+
+  # Make all subdirectories under the gEDA directory
+
+  mkdir -m 0777 -p bitmap/24x24
+  mkdir -m 0777 -p bitmap/26x26
+  mkdir -m 0777 -p bitmap/28x28
+  mkdir -m 0777 -p scheme/geda
+  mkdir -m 0777 -p scheme/gschem
+  mkdir -m 0777 -p gafrc.d
+  mkdir -m 0777 -p icons/hicolor/22x22/actions
+
+  vecho "GEDADATARC=$GEDADATARC"
+
+  cd $CWDSAVE
+
+  # Create links to bitmaps
+
+  # Note that the toplevel bitmaps are setup first because the images
+  # from gschem/do not get sort into subdirectories
+  if [ -d $SRCDIR/../../bitmaps ] ; then
+     cd $SRCDIR/../../bitmaps
+     ln -s $PWD/*.png $GEDADATARC/bitmap/
+     ln -s $PWD/*.xpm $GEDADATARC/bitmap/
+     cd $GEDADATARC/bitmap/
+     mv *24x24.* 24x24/
+     mv *26x26.* 26x26/
+     mv *28x28.* 28x28/
+     cd $CWDSAVE
+  else
+    echo "Error: not in the right place, cannot find toplevel bitmaps"
+    exit 0
+  fi
+
+  if [ -d $SRCDIR/../bitmap ] ; then
+     cd $SRCDIR/../bitmap
+     ln -s $PWD/*.png $GEDADATARC/bitmap/
+     ln -s $PWD/*.xpm $GEDADATARC/bitmap/
+     cd $CWDSAVE
+  else
+    echo "Error: not in the right place, cannot find gschem bitmaps"
+    exit 0
+  fi
+
+  # Make links to rc files
+  if [ -d $SRCDIR/../etc ] ; then
+     cd $SRCDIR/../etc
+     ln -s $PWD/gschem-* $GEDADATARC/
+     ln -s $PWD/system-gschemrc $GEDADATARC/
+     cd $CWDSAVE
+  else
+    echo "Error: not in the right place, cannot find gschem etc directory"
+    exit 0
+  fi
+
+  if [ -d $SRCDIR/../../libgeda/etc ] ; then
+     cd $SRCDIR/../../libgeda/etc
+     ln -s $PWD/print-* $GEDADATARC/
+     ln -s $PWD/system-gafrc $GEDADATARC/
+     cd $CWDSAVE
+  else
+    echo "Error: not in the right place, cannot find libgeda etc directory"
+    exit 0
+  fi
+
+  # Copy RC files
+  if [ -d $SRCDIR/../scheme ] ; then
+     cd $SRCDIR/../scheme
+     cp -n $PWD/*.scm $GEDADATARC/scheme/
+     cp -n $PWD/gschem/*.scm $GEDADATARC/scheme/gschem/
+     cd $CWDSAVE
+  else
+    echo "Error: not in the right place, cannot find gschem scheme"
+    exit 0
+  fi
+
+  if [ -d $SRCDIR/../../libgeda/scheme ] ; then
+     cd $SRCDIR/../../libgeda/scheme
+     cp -n $PWD/*.scm $GEDADATARC/scheme/
+     cp -n $PWD/geda/*.scm $GEDADATARC/scheme/geda/
+     cd $CWDSAVE
+  else
+    echo "Error: not in the right place, cannot find libgeda scheme directory"
+    exit 0
+  fi
+
+  if [ -d $SRCDIR/../../symbols ] ; then
+   cd $SRCDIR/../../symbols
+   export SYMDIR=$PWD
+   vecho "SYMDIR=$SYMDIR"
+   cd $GEDADATARC/gafrc.d/
+   ln -s $SYMDIR/geda-clib.scm geda-clib.scm
+   cd ..
+   ln -s $SYMDIR sym
+   cd $CWDSAVE
+  else
+    echo "Error: not in the right place, cannot find symbols directory"
+    exit 0
+  fi
+
+  if [ -d $SRCDIR/../data ] ; then
+     cd $SRCDIR/../data
+     ln -s $PWD/*22.png $GEDADATARC/icons/hicolor/22x22/actions/
+     cd  $GEDADATARC/icons/hicolor/22x22/actions/
+     for i in *.png ; do mv "$i" "${i/-22.png/.png}" ; done
+     cd $CWDSAVE
+  else
+    echo "Error: not in the right place, cannot find toplevel bitmaps"
+    exit 0
+  fi
+
+  cd $CWDSAVE
+}
+
+03_setup_guild_environment ()
+{
+  local CWDSAVE=$PWD
+
+  if [ -d $BUILDDIR/../scheme ] ; then
+   cd $BUILDDIR/../scheme
+   export GUILE_LOAD_PATH=$PWD:$GUILE_LOAD_PATH
+   cd $BUILDDIR/../../libgeda/scheme
+   export GUILE_LOAD_PATH=$PWD:$GUILE_LOAD_PATH
+   cd $CWDSAVE
+   vecho "GUILE_LOAD_PATH=$GUILE_LOAD_PATH"
+  else
+    echo "Error: not in the right place, cannot find scheme directory"
+    exit 0
+  fi
+}
+
+04_setup_test_environment()
 {
    mkdir -m 0777 -p "logs"
 
@@ -157,7 +332,10 @@ if test ! -x $APPLICATION ; then
   echo "Error: Can not find $APPLICATION"
   exit 1;
 else
-  APPLICATION=${BUILDDIR}/../../src/.libs/$PROGRAM
+
+  # Note: Tested for gschem one level up but gschem will be ran
+  # from the run directory, which will be 1 level down, so:
+  APPLICATION="${BUILDDIR}/../../src/.libs/$PROGRAM"
 fi
 
 # Check that the inputs directory exists
@@ -168,7 +346,7 @@ fi
 
 # --------------------- Get test input files --------------------
 #
-# If BASE_NAME is nil then not single test mode
+# If BASE_NAME is nil then NOT single test mode
 #
 if [ -z "$BASE_NAME" ] ; then
   # Remove any old files from the input directory
@@ -182,7 +360,10 @@ if [ ${#INPUTS[@]} -eq 0 ] ; then
   exit 1;
 fi
 
-do_setup_environment
+01_setup_path2libraries
+02_setup_geda_environment
+03_setup_guild_environment
+04_setup_test_environment
 
 # --------------- Process each of the input files ---------------
 
@@ -191,7 +372,7 @@ for file in $INPUTS ; do
   TOTALTEST=$(($TOTALTEST + $?))
 done
 
-if $REGENERATE ; then
+if ${REGENERATE} ; then
   vecho "Regenerated $TOTALTEST test"
 else
   echo "TOTALTEST=$TOTALTEST PASSCOUNT=$PASSCOUNT FAILCOUNT=$FAILCOUNT"
@@ -200,6 +381,8 @@ fi
 # Clean up if not debugging
 if ! $DEBUG ; then
   rm -rf ${RUNDIR}
+  rm -rf libs
+  rm -rf gEDA
 else
   image_func_config
 fi
