bad41e6c4c38ebcad07b9e2c25513938e5b36652
WileyECoyote <wileyhill@gmail.com>
2015-12-05 20:27:41 -0600

    gattrib: Replace fprintf with u_log_qmessage

--- gattrib/src/x_window.c
+++ gattrib/src/x_window.c
@@ -201,7 +201,7 @@ void x_window_restore_settings(GtkWidget *MainWindow)
   x = eda_config_get_integer (cfg, group_name, "window-x-position", &err);
 
   if (err != NULL) {
-    fprintf(stderr, "Error retrieving user configuration: '%s'\n", err->message);
+    u_log_qmessage("%s\n", err->message);
     g_clear_error (&err);
     xy_error = TRUE;
   }
