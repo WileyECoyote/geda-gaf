8edf2ef287fe4d645b78097e78ead88f4f394a6e
WileyECoyote <wileyhill@gmail.com>
2015-12-20 14:05:46 -0600

    Update header in headers, upgrade GNU add GNU WEB link

--- gschem/src/x/x_guile.c
+++ gschem/src/x/x_guile.c
@@ -1,3 +1,28 @@
+/* -*- C indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-
+ *
+ * File: x_guile.c
+ *
+ * gEDA - GPL Electronic Design Automation
+ * gschem - gEDA Schematic Capture
+ *
+ * Copyright (C) 2015 Wiley Edward Hill
+ * Copyright (C) 2015 gEDA Contributors (see ChangeLog for details)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA, <http://www.gnu.org/licenses/>.
+ */
 
 #include <gschem.h>
 #include <x_dialog.h>
