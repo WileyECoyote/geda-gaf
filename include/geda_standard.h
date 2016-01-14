1976d74aae78eb239e1a2a1c7d9659b8ba89cc0b
WileyECoyote <wileyhill@gmail.com>
2016-01-14 02:14:56 -0600

    tweak conditional for inclusion of config

--- include/geda_standard.h
+++ include/geda_standard.h
@@ -24,31 +24,39 @@
  *  Contributing Author: Wiley Edward Hill
  *  Date Contributed: July, 25, 2014
  */
+/*!
+ * \file geda_standard.h
+ * \brief Include standard C library headers
+ * \par
+ *  This file includes commonly used standard library headers and is used
+ *  to reduce clutter so that specific inclusion required for source files
+ *  are not obscured.
+ */
 #ifndef __GEDA_STD_INC__
 #  define __GEDA_STD_INC__
 
+#  ifdef HAVE_CONFIG_H
+#    include <config.h>
+#  endif
+
 #  include <ascii.h>
 
-#  ifdef HAVE_CONFIG_H
+#  include <stdio.h>
 
-#    include "config.h"
-
-#    include <stdio.h>
-
-#    ifdef HAVE_STRING_H
-#       include <string.h>
-#    endif
-
-#    ifdef HAVE_STDLIB_H
-#      include <stdlib.h>
-#    endif
-
-#    ifdef HAVE_UNISTD_H
-#      include <unistd.h>
-#    endif
-
-#    ifdef HAVE_ERRNO_H
-#      include <errno.h>
-#    endif
+#  ifdef HAVE_STRING_H
+#     include <string.h>
 #  endif
-#endif
+
+#  ifdef HAVE_STDLIB_H
+#    include <stdlib.h>
+#  endif
+
+#  ifdef HAVE_UNISTD_H
+#     include <unistd.h>
+#  endif
+
+#  ifdef HAVE_ERRNO_H
+#     include <errno.h>
+#  endif
+
+#endif /* __GEDA_STD_INC__ */
