475d2acb32921fcf5f0bf6e581af25febccfd3cb
WileyECoyote <wileyhill@gmail.com>
2016-01-19 02:13:31 -0600

    Revert "enum.h: revise header, edit comments, adjust indentations"
    This reverts commit 6daa218505ec981ed9284d5c99cbcf5a7682cca6.

--- include/geda_enum.h
+++ include/geda_enum.h
@@ -4,33 +4,33 @@
  *
  * gEDA - GPL Electronic Design Automation
  *
- * Copyright (C) 2013-2016 Wiley Edward Hill
+ * Copyright (C) 2013-2015 Wiley Edward Hill
  *
- * This software is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This software is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this software; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02111-1301 USA, <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA, <http://www.gnu.org/licenses/>.
  */
 
 /* ------------------------------------------------------------------ */
 
-#ifndef GEDA_ENUMERATED_H
-#define GEDA_ENUMERATED_H
+#ifndef __GEDA_ENUMERATED__
+#define __GEDA_ENUMERATED__
 
 /** \file geda_enum.h
  *
  *   \defgroup geda-global-enumerators Global Enumerated Semi-Constants
- * @{\par This group contains enumerations used through-out the gEDA suite.
+ * @{\par This group contains Macros for controls used in Dialogs
  *   \ingroup (geda-globals)
  */
 
@@ -187,5 +187,4 @@ typedef enum
 } IDE_SIZE_REQUEST;
 
 /** @} endgroup geda-global-enumerators */
-
-#endif /* GEDA_ENUMERATED_H */
+#endif
