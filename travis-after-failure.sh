3a6106f8d4b3461140f0249600442afa93a7534f
WileyECoyote <wileyhill@gmail.com>
2016-01-20 07:33:01 -0600

    tweak travis somemore

--- travis-after-failure.sh
+++ travis-after-failure.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-test -f ./libgedathon/tests/test-suite.log && cat libgedathon/tests/test-suite.log
 
-cat ./libgedathon/tests/run_tests.sh.log
+test -f ./libgedathon/tests/test-suite.log && cat libgedathon/tests/test-suite.log
+test -f ./libgedathon/tests/run_tests.sh.log && cat ./libgedathon/tests/run_tests.sh.log
+
