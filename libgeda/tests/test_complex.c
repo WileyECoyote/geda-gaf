c7cf65e8c4507357ad40bcc2525fdf1908dcfb4f
WileyECoyote <wileyhill@gmail.com>
2016-02-01 04:10:57 -0600

    Add tests for testing geda_complex module

---
+++ libgeda/tests/test_complex.c
@@ -0,0 +1,117 @@
+#include <glib.h>
+#include <libgeda.h>
+
+#define TOBJECT "GedaComplex"
+
+/*! \file tests_circle.c
+ *  \brief Tests for geda_complex.c module
+ */
+
+int test_complex (void)
+{
+  int result = 0;
+
+  GedaObject *object = geda_complex_new();
+
+  if (!GEDA_IS_OBJECT(object)) {
+    fprintf(stderr, "%s: is a GedaObject Failed\n", TOBJECT);
+    result++;
+  }
+
+  /* The one with the Not operator is the one being tested */
+
+  if (GEDA_IS_ARC(object)) {
+    fprintf(stderr, "%s matched type GedaArc\n", TOBJECT);
+    result++;
+  }
+
+  if (GEDA_IS_BOX(object)) {
+    fprintf(stderr, "%s matched type GedaBox\n", TOBJECT);
+    result++;
+  }
+
+  if (GEDA_IS_BUS(object)) {
+    fprintf(stderr, "%s matched type GedaBus\n", TOBJECT);
+    result++;
+  }
+
+  if (GEDA_IS_CIRCLE(object)) {
+    fprintf(stderr, "%s matched type GedaCircle\n", TOBJECT);
+    result++;
+  }
+
+  if (!GEDA_IS_COMPLEX(object)) {
+    fprintf(stderr, "is a %s Failed in %s\n", TOBJECT, __func__);
+    result++;
+  }
+
+  if (GEDA_IS_LINE(object)) {
+    fprintf(stderr, "%s matched type GedaLine\n", TOBJECT);
+    result++;
+  }
+
+  if (GEDA_IS_NET(object)) {
+    fprintf(stderr, "%s: matched type GedaNet\n", TOBJECT);
+    result++;
+  }
+
+  if (GEDA_IS_PATH(object)) {
+    fprintf(stderr, "%s matched type GedaPath\n", TOBJECT);
+    result++;
+  }
+
+  if (GEDA_IS_PICTURE(object)) {
+    fprintf(stderr, "%s matched type GedaPicture\n", TOBJECT);
+    result++;
+  }
+
+  if (GEDA_IS_PIN(object)) {
+    fprintf(stderr, "%s matched type GedaPin\n", TOBJECT);
+    result++;
+  }
+
+  if (GEDA_IS_TEXT(object)) {
+    fprintf(stderr, "%s matched type GedaText\n", TOBJECT);
+    result++;
+  }
+
+  GedaComplex *complex = object->complex;
+
+  if (!GEDA_IS_COMPLEX(complex)) {
+    fprintf(stderr, "sub-pointer is a %s Failed\n", TOBJECT);
+    result++;
+  }
+  else if (object->type != OBJ_COMPLEX) {
+    fprintf(stderr, "%s type not %c\n", TOBJECT, OBJ_COMPLEX);
+    result++;
+  }
+
+  g_object_unref(object);
+
+  if (GEDA_IS_COMPLEX(object)) {
+    fprintf(stderr, "%s was not destroyed\n", TOBJECT);
+    result++;
+  }
+
+  if (GEDA_IS_OBJECT(object)) {
+    fprintf(stderr, "%s parent was not destroyed\n", TOBJECT);
+    result++;
+  }
+
+  return result;
+}
+
+int
+main (int argc, char *argv[])
+{
+  int result = 0;
+
+  /* Initialize gobject */
+#if (( GLIB_MAJOR_VERSION == 2 ) && ( GLIB_MINOR_VERSION < 36 ))
+  g_type_init();
+#endif
+
+  result = test_complex();
+
+  return result > 0;
+}
