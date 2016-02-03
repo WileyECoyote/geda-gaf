607063df723807809a8034d1a9a83dc807991822
WileyECoyote <wileyhill@gmail.com>
2016-02-03 00:23:47 -0600

    travis: add to after failure script

--- travis-after-failure.sh
+++ travis-after-failure.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+test -f ./libgeda/tests/test-suite.log && cat ./libgeda/tests/test-suite.log
+
 test -f ./libgedathon/tests/test-suite.log && cat libgedathon/tests/test-suite.log
 test -f ./libgedathon/tests/run_tests.sh.log && cat ./libgedathon/tests/run_tests.sh.log
 
+test -f ./gschem/tests/test-suite.log && cat ./gschem/tests/test-suite.log
