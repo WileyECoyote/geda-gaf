f413a7698681d9fbe98e416df70d23e0f09b3f2d
WileyECoyote <wileyhill@gmail.com>
2016-02-27 18:37:26 -0600

    gschem: show error message when gtk_init_check fails

--- gschem/src/base/gschem.c
+++ gschem/src/base/gschem.c
@@ -432,13 +432,13 @@ static void main_prog(void *closure, int argc, char *argv[])
   XInitThreads();
 
 #ifdef G_STAT_MEM
-     g_mem_set_vtable(glib_mem_profiler_table);
+  g_mem_set_vtable(glib_mem_profiler_table);
 #else
-     geda_utility_program_mem_set_vtable();
+  geda_utility_program_mem_set_vtable();
 #endif
 
 #if ((GLIB_MAJOR_VERSION >= 2 ) && (GLIB_MINOR_VERSION <= 33 ))
-     g_slice_set_config(G_SLICE_CONFIG_ALWAYS_MALLOC, 1);
+  g_slice_set_config(G_SLICE_CONFIG_ALWAYS_MALLOC, 1);
 #endif
 
 #ifdef HAVE_GTHREAD
@@ -447,7 +447,7 @@ static void main_prog(void *closure, int argc, char *argv[])
 
 #if ((GLIB_MAJOR_VERSION == 2 ) && (GLIB_MINOR_VERSION < 32 ))
 
-     g_thread_init (NULL);
+  g_thread_init (NULL);
 
 #endif
 
@@ -500,6 +500,10 @@ static void main_prog(void *closure, int argc, char *argv[])
 
     gschem_quit();
     v_log_message(_("Exiting normal\n"));
+  }
+  else {
+    fprintf (stderr, "FAILED: gtk_init_check\n");
+    exit 1;
   }
 }
 
