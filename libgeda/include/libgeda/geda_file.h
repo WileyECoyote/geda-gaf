e7e6c1fc82d89e946971a9abab66a3e293e8c672
WileyECoyote <wileyhill@gmail.com>
2016-03-06 23:38:36 -0600

    libgeda: add convenience "user" macros

---
+++ libgeda/include/libgeda/geda_file.h
@@ -0,0 +1,80 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4 tab-width: 4 -*- */
+/*
+ * File: geda_utility.h
+ *
+ * gEDA - GPL Electronic Design Automation
+ * libgeda - gEDA's library
+ *
+ * Copyright (C) 2013-2015 Wiley Edward Hill
+ * Copyright (C) 2013-2015 gEDA Contributors (see ChangeLog for details)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA, <http://www.gnu.org/licenses/>.
+ *
+ *  Contributing Author: Wiley Edward Hill
+ *  Date Contributed: November, 18, 2013
+ */
+/*! \file geda_file.h "libgeda/geda_file.h"
+ *  \brief Libgeda macros for file modules.
+ *  \todo functions names in the file subdirectories have still
+          using f_prefix
+ */
+
+#ifndef __GEDA_FILE_MAC__
+#define __GEDA_FILE_MAC__
+
+/* f_file.c */
+#define geda_close_file           f_close
+#define geda_file_has_autosave    f_has_active_autosave
+#define geda_open_file            f_open
+#define geda_file_open_flags      f_open_flags
+#define geda_remove_backup_file   f_remove_backup_file
+#define geda_save_file            f_save
+
+/* f_get.c */
+#define geda_get_autosave_name    f_get_autosave_filename
+#define geda_get_basename         f_get_basename
+#define geda_get_bitmap_spec      f_get_bitmap_filespec
+#define geda_get_data_spec        f_get_data_filespec
+#define geda_get_dir_list         f_get_dir_list_files
+#define geda_get_file_contents    f_get_file_contents
+#define geda_get_extension        f_get_filename_ext
+#define geda_get_format_header    f_get_format_header
+#define geda_is_path_absolute     f_get_is_path_absolute
+
+/* f_path.c */
+#define geda_create_path          f_path_create
+#define geda_free_path            f_path_free
+#define geda_get_dirname          f_path_get_dirname
+#define geda_sys_data_path        f_path_sys_data
+#define geda_sys_doc_path         f_path_sys_doc
+#define geda_sys_config_path      f_path_sys_config
+#define geda_user_config_path     f_path_user_config
+
+/* f_print.c */
+#define geda_print_file           f_print_file
+#define geda_print_command        f_print_command;
+#define geda_print_stream         f_print_stream
+#define geda_set_print_type       f_print_set_type
+
+/* f_sys.c */
+#define geda_copy_file            f_sys_copy
+#define geda_cmp_file_mod_time    f_sys_cmp_mod_time
+#define geda_follow_symlinks      f_sys_follow_symlinks
+#define geda_normalize_name       f_sys_normalize_name
+#define geda_remove_file          f_sys_remove
+#define geda_remove_extension     f_sys_remove_extension
+
+#endif /* __GEDA_FILE_MAC__ */
---
+++ libgeda/include/libgeda/geda_utility.h
@@ -0,0 +1,102 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4 tab-width: 4 -*- */
+/*
+ * File: geda_utility.h
+ *
+ * gEDA - GPL Electronic Design Automation
+ * libgeda - gEDA's library
+ *
+ * Copyright (C) 2016 gEDA Contributors (see ChangeLog for details)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA, <http://www.gnu.org/licenses/>.
+ *
+ *  Contributing Author: Wiley Edward Hill
+ *  Date Contributed: March, 6th, 2016
+ */
+/*! \file geda_utility.h "libgeda/geda_utility.h"
+ *  \brief Libgeda macros for utility modules.
+ *
+ */
+
+#ifndef __GEDA_UTILITIES_MAC__
+#define __GEDA_UTILITIES_MAC__
+
+/* u_utility.c */
+#define geda_expand_env_variable  geda_utility_expand_env_variable
+#define geda_print_object         geda_utility_program_print_object
+
+/* u_glist.c */
+#define geda_clear_glist          geda_utility_glist_clear
+#define geda_glist_find_string    geda_utility_glist_find_string
+#define geda_glist_free_all       geda_utility_glist_free_all
+#define geda_glist_free_full      geda_utility_glist_free_full
+#define geda_glist_str_inlist     geda_utility_glist_str_inlist
+#define geda_glist_stri_inlist    geda_utility_glist_stri_inlist
+
+#define geda_clear_gslist         geda_utility_gslist_clear
+#define geda_gslist_find_string   geda_utility_gslist_find_string
+#define geda_gslist_free_all      geda_utility_gslist_free_all
+#define geda_gslist_free_full     geda_utility_gslist_free_full
+#define geda_gslist_str_inlist    geda_utility_gslist_str_inlist
+#define geda_gslist_stri_inlist   geda_utility_gslist_stri_inlist
+
+/* u_log.c */
+#define geda_close_log            geda_utility_log_close
+#define geda_get_log_time         geda_utility_log_get_log_time
+#define geda_init_log             geda_utility_log_init
+#define geda_quite_log            geda_utility_log_quite
+#define geda_read_log             geda_utility_log_read
+#define geda_set_log_time         geda_utility_log_set_log_time
+#define geda_set_log_update_func  geda_utility_log_set_update_func
+#define geda_system_log           geda_utility_log_system
+#define geda_verbose_log          geda_utility_log_verbose
+
+/* u_string.c */
+#define geda_strconcat            geda_utility_string_concat
+#define geda_strconcat            geda_utility_string_concat
+#define geda_string_int2str       geda_utility_string_int2str
+#define geda_string_isalnum       geda_utility_string_isalnum
+#define geda_string_istr          geda_utility_string_istr
+#define geda_string_parse_xy      geda_utility_string_parse_xy
+#define geda_remove_last_newline  geda_utility_string_remove_last_nl
+#define geda_remove_newline       geda_utility_string_remove_nl
+#define geda_string_scm2c         geda_utility_string_scm2c
+#define geda_sort_string_array    geda_utility_string_sort_array
+#define geda_strsplit             geda_utility_string_split
+#define geda_sprintf              geda_utility_string_sprintf
+#define geda_strdup               geda_utility_string_strdup
+#define geda_strequal             geda_utility_string_strequal
+#define geda_stricmp              geda_utility_string_stricmp
+#define geda_stristr              geda_utility_string_stristr
+#define geda_strisubst            geda_utility_string_strisubst
+#define geda_strncmpi             geda_utility_string_strncmpi
+#define geda_strndup              geda_utility_string_strndup
+#define geda_strsize              geda_utility_string_strsize
+#define geda_strstr_rep           geda_utility_string_strstr_rep
+#define geda_strsubst             geda_utility_string_strsubst
+#define geda_word_count           geda_utility_string_word_count
+
+/* u_program.c */
+#define geda_program_backtrace    geda_utility_program_backtrace
+#define geda_set_memory_vtable    geda_utility_program_mem_set_vtable
+
+/* u_refdes.c */
+#define geda_get_ieee_refdes      geda_utility_refdes_get_ieee
+#define geda_get_standard_refdes  geda_utility_refdes_get_standard
+#define geda_get_spice_refdes     geda_utility_refdes_get_spice
+#define geda_reset_refdes         geda_utility_refdes_reset
+#define geda_refdes_get_numeric   geda_utility_refdes_return_numeric
+
+#endif /* __GEDA_UTILITIES_MAC__ */
