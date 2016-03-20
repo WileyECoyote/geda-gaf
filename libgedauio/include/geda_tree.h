c540c1a6b82b48b7f53916f11e31260a3142adef
WileyECoyote <wileyhill@gmail.com>
2016-03-20 14:56:55 -0500

    libgedauio: upgrade header, add upgrade option

--- libgedauio/include/geda_tree.h
+++ libgedauio/include/geda_tree.h
@@ -6,22 +6,22 @@
  * gEDA - GPL Electronic Design Automation
  * libgedauio - gEDA's library for User Interface Objects
  *
- * Copyright (C) 2015 Wiley Edward Hill
+ * Copyright (C) 2015-2016 Wiley Edward Hill
  *
- * This Library is free software; you can redistribute it and or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; version 3 of the
- * License.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
  *
- * This Library is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301 USA <http://www.gnu.org/licenses/>.
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02111-1301 USA, <http://www.gnu.org/licenses/>.
  *
  * Date: January 10, 2015
  * Contributing Author: Wiley Edward Hill <wileyhill@gmail.com>
