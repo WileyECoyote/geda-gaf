b8b83033c27f8489d8dfa14139b8be8d11a8080b
WileyECoyote <wileyhill@gmail.com>
2016-03-31 23:17:22 -0500

    gschem: remove useless conditional, update header

--- gschem/src/base/gschem.c
+++ gschem/src/base/gschem.c
@@ -1,11 +1,11 @@
 /* gEDA - GPL Electronic Design Automation
  * gschem - gEDA Schematic Capture
- * Copyright (C) 1998-2015 Ales Hvezda
- * Copyright (C) 1998-2015 gEDA Contributors (see ChangeLog for details)
+ * Copyright (C) 1998-2016 Ales Hvezda
+ * Copyright (C) 1998-2016 gEDA Contributors (see ChangeLog for details)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -259,17 +259,14 @@ load_documents(GschemToplevel *w_current, int argv_index, int argc, char *argv[]
       }
       else {
 
-        /* Lastly, if still no page has been loaded, start with blank page */
-        if ( !page_loaded ) {
-          x_window_open_page( w_current, NULL );
-        }
+        /* Still no page has been loaded, start with blank page */
+        x_window_open_page( w_current, NULL );
       }
     }
   }
 
   /* Update the window to show the current page */
   x_window_set_current_page( w_current, Current_Page);
-
 }
 
 static void gschem( int argc, char *argv[])
