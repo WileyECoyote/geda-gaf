5b670e4e552a171f1f2d815555ccb45f281a0e96
WileyECoyote <wileyhill@gmail.com>
2016-04-15 23:37:31 -0500

    libgedauio/test: add tests of GedaEntry constructors

---
+++ libgedauio/tests/test_entry.c
@@ -0,0 +1,156 @@
+/* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 2 tab-width: 2 -*- */
+/* vi: set et ts=2 sw=2 sts=2: */
+/*
+ * File: test_entry.c
+ *
+ * gEDA - GPL Electronic Design Automation
+ * libgedauio - gEDA's library for User Interface Objects
+ *
+ * Copyright (C) 2016 gEDA Contributors (see ChangeLog for details)
+ *
+ * This Library is free software; you can redistribute it and or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; version 3 of the
+ * License.
+ *
+ * This Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA <http://www.gnu.org/licenses/>.
+ *
+ * Date: April, 13, 2016
+ * Contributing Author: Wiley Edward Hill
+ */
+
+#include "config.h"
+
+#include <string.h>
+
+#include <glib.h>
+#include <gtk/gtk.h>
+
+#include <geda/geda.h>
+
+#include <../include/geda_entry.h>
+#include <../include/geda_bulb.h>
+
+#define TWIDGET "GedaEntry"
+
+/*! \file test_entry.c
+ *  \brief Tests for geda_entry.c module
+ */
+
+int check_construction (void)
+{
+  int result = 0;
+
+  /* geda_entry_new */
+
+  GtkWidget *widget = geda_entry_new (NO_HISTORY, NO_COMPLETION);
+
+  if (!GEDA_IS_ENTRY(widget)) {
+    fprintf(stderr, "FAILED: line <%d> is a %s\n", __LINE__, TWIDGET);
+    result++;
+  }
+
+  if (!GTK_IS_ENTRY(widget)) {
+    fprintf(stderr, "FAILED: line <%d> is a %s\n", __LINE__, TWIDGET);
+    result++;
+  }
+
+  if (GEDA_IS_BULB(widget)) {
+    fprintf(stderr, "FAILED: line <%d> is a %s\n", __LINE__, TWIDGET);
+    result++;
+  }
+
+  g_object_ref_sink(widget); /* Sink reference to entry widget */
+  g_object_unref(widget);    /* Destroy the widget */
+
+  if (GEDA_IS_ENTRY(widget)) {
+    fprintf(stderr, "FAILED %s destruction\n", TWIDGET);
+    result++;
+  }
+
+  widget = NULL;
+
+  /* geda_entry_new_visible */
+
+  widget = geda_entry_new_visible (NO_HISTORY, NO_COMPLETION);
+
+  if (!GEDA_IS_ENTRY(widget)) {
+    fprintf(stderr, "FAILED: line <%d> is a %s\n", __LINE__, TWIDGET);
+    result++;
+  }
+
+  g_object_ref_sink(widget); /* Sink reference to entry widget */
+  g_object_unref(widget);    /* Destroy the widget */
+
+  widget = NULL;
+
+  /* geda_entry_new_with_buffer */
+
+  GtkEntryBuffer *buffer;
+
+  widget = geda_entry_new_with_buffer (NULL);
+
+  if (GEDA_IS_ENTRY(widget)) {
+    fprintf(stderr, "FAILED: line <%d> is a %s\n", __LINE__, TWIDGET);
+    result++;
+  }
+
+  buffer = gtk_entry_buffer_new ("abc", 6);
+
+  widget = geda_entry_new_with_buffer (buffer);
+
+  if (!GEDA_IS_ENTRY(widget)) {
+    fprintf(stderr, "FAILED: line <%d> is a %s\n", __LINE__, TWIDGET);
+    result++;
+  }
+
+  g_object_ref_sink(widget); /* Sink reference to entry widget */
+  g_object_unref(widget);    /* Destroy the widget */
+
+  widget = NULL;
+
+  /* geda_entry_new_with_max_length */
+
+  widget = geda_entry_new_with_max_length (10);
+
+  if (!GEDA_IS_ENTRY(widget)) {
+    fprintf(stderr, "FAILED: line <%d> is a %s\n", __LINE__, TWIDGET);
+    result++;
+  }
+
+  g_object_ref_sink(widget); /* Sink reference to entry widget */
+  g_object_unref(widget);    /* Destroy the widget */
+
+  return result;
+}
+
+int
+main (int argc, char *argv[])
+{
+  int result = 0;
+  int subtotal = 0;
+
+  /* Initialize gobject */
+#if (( GLIB_MAJOR_VERSION == 2 ) && ( GLIB_MINOR_VERSION < 36 ))
+  g_type_init();
+#endif
+
+  if (gtk_init_check(&argc, &argv)) {
+
+    subtotal = check_construction();
+    if (subtotal) {
+      fprintf(stderr, "Check %s constructors", TWIDGET);
+      result   = subtotal;
+      subtotal = 0;
+    }
+  }
+  return result;
+}
