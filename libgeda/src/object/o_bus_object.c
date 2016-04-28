92536c75732835edd191ca2331b579e30d56bc15
WileyECoyote <wileyhill@gmail.com>
2016-04-28 00:27:03 -0500

    libgeda: add common o_bus message error message handler

--- libgeda/src/object/o_bus_object.c
+++ libgeda/src/object/o_bus_object.c
@@ -31,6 +31,12 @@
 static int  geda_bus_object_consolidate_segments (GedaObject *object) __attribute__((unused));
 static void geda_bus_object_consolidate_lowlevel (GedaObject *object,  GedaObject *del_object, int orient) __attribute__((unused));
 
+static void
+geda_object_error(const char *func, const void *object, IDE_OBJECT_TYPE type)
+{
+  geda_error_object_argument(__FILE__, func, object, type);
+}
+
 /* \brief
  * \par Function Description
  * This function does the actual work of making one bus segment out of two
@@ -170,20 +176,23 @@ geda_bus_object_copy(const GedaObject *o_current)
 {
   GedaObject *new_obj;
 
-  g_return_val_if_fail(GEDA_IS_BUS(o_current), NULL);
+  if (GEDA_IS_BUS(o_current)) {
 
-  /* make sure you fix this in pin and bus as well */
-  /* still doesn't work... you need to pass in the new values */
-  /* or don't update and update later */
-  /* I think for now I'll disable the update and manually update */
-  new_obj = geda_bus_object_new (o_current->color,
-                       o_current->line->x[0], o_current->line->y[0],
-                       o_current->line->x[1], o_current->line->y[1],
-                       o_current->bus->bus_ripper_direction);
+    /* make sure you fix this in pin and bus as well */
+    /* still doesn't work... you need to pass in the new values */
+    /* or don't update and update later */
+    /* I think for now I'll disable the update and manually update */
+    new_obj = geda_bus_object_new (o_current->color,
+                                   o_current->line->x[0], o_current->line->y[0],
+                                   o_current->line->x[1], o_current->line->y[1],
+                                   o_current->bus->bus_ripper_direction);
 
-  new_obj->line_options->line_width = *o_current->bus->line_width;
+    new_obj->line_options->line_width = *o_current->bus->line_width;
 
-  return new_obj;
+    return new_obj;
+  }
+  geda_object_error(__func__, o_current, GEDA_OBJECT_BUS);
+  return NULL;
 }
 
 /*!
@@ -198,25 +207,31 @@ geda_bus_object_get_direction(const GedaObject *object)
 {
   int direction = 0;
 
-  if (object->line->x[1] > object->line->x[0] &&
+  if (GEDA_IS_BUS(object)) {
+
+    if (object->line->x[1] > object->line->x[0] &&
       object->line->y[1] == object->line->y[0] )
-  {
-    direction = 1;
+    {
+      direction = 1;
+    }
+    else if (object->line->x[0] > object->line->x[1] &&
+      object->line->y[1] == object->line->y[0])
+    {
+      direction = -1;
+    }
+    else if (object->line->x[0] == object->line->x[1] &&
+      object->line->y[1] > object->line->y[0])
+    {
+      direction = 1;
+    }
+    else if (object->line->x[0] == object->line->x[1] &&
+      object->line->y[0] > object->line->y[1])
+    {
+      direction = -1;
+    }
   }
-  else if (object->line->x[0] > object->line->x[1] &&
-           object->line->y[1] == object->line->y[0])
-  {
-    direction = -1;
-  }
-  else if (object->line->x[0] == object->line->x[1] &&
-           object->line->y[1] > object->line->y[0])
-  {
-    direction = 1;
-  }
-  else if (object->line->x[0] == object->line->x[1] &&
-           object->line->y[0] > object->line->y[1])
-  {
-    direction = -1;
+  else {
+    geda_object_error(__func__, object, GEDA_OBJECT_BUS);
   }
   return direction;
 }
@@ -235,6 +250,7 @@ geda_bus_object_get_ripper_direction (const GedaObject *object)
   if (GEDA_IS_BUS(object)) {
     return object->bus->bus_ripper_direction;
   }
+  geda_object_error(__func__, object, GEDA_OBJECT_BUS);
   return 0;
 }
 
@@ -252,36 +268,44 @@ geda_bus_object_get_ripper_direction (const GedaObject *object)
 bool
 geda_bus_object_get_position(GedaObject *object, int *x, int *y)
 {
-  g_return_val_if_fail(GEDA_IS_BUS(object), FALSE);
+  if (GEDA_IS_BUS(object)) {
+    *x = object->line->x[0];
+    *y = object->line->y[0];
 
-  *x = object->line->x[0];
-  *y = object->line->y[0];
-
-  return TRUE;
+    return TRUE;
+  }
+  geda_object_error(__func__, object, GEDA_OBJECT_BUS);
+  return FALSE;
 }
 
-/*! \brief mirror a bus object horizontaly at a centerpoint
- *
- *  \par Function Description
+/*!
+ * \brief mirror a bus object horizontaly at a centerpoint
+ * \par Function Description
  *  This function mirrors a bus \a object horizontaly at the point
  *  (\a center_x, \a center_y).
  *
- *  \param [in,out] object    The bus object
- *  \param [in]     center_x  x-coord of the mirror position
- *  \param [in]     center_y  y-coord of the mirror position
+ * \param [in,out] object    The bus object
+ * \param [in]     center_x  x-coord of the mirror position
+ * \param [in]     center_y  y-coord of the mirror position
 
  */
 void
 geda_bus_object_mirror(GedaObject *object, int center_x, int center_y)
 {
-  /* translate object to origin */
-  geda_bus_object_translate(object, -center_x, -center_y);
+  if (GEDA_IS_BUS(object)) {
 
-  object->line->x[0] = -object->line->x[0];
+    /* translate object to origin */
+    geda_bus_object_translate(object, -center_x, -center_y);
 
-  object->line->x[1] = -object->line->x[1];
+    object->line->x[0] = -object->line->x[0];
 
-  geda_bus_object_translate(object, center_x, center_y);
+    object->line->x[1] = -object->line->x[1];
+
+    geda_bus_object_translate(object, center_x, center_y);
+  }
+  else {
+    geda_object_error(__func__, object, GEDA_OBJECT_BUS);
+  }
 }
 
 /*!
@@ -299,12 +323,18 @@ geda_bus_object_mirror(GedaObject *object, int center_x, int center_y)
 void
 geda_bus_object_modify(GedaObject *object, int x, int y, int whichone)
 {
-  object->line->x[whichone] = x;
-  object->line->y[whichone] = y;
+  if (GEDA_IS_BUS(object)) {
 
-  s_tile_update_object(object);
+    object->line->x[whichone] = x;
+    object->line->y[whichone] = y;
 
-  object->w_bounds_valid_for = NULL;
+    s_tile_update_object(object);
+
+    object->w_bounds_valid_for = NULL;
+  }
+  else {
+    geda_object_error(__func__, object, GEDA_OBJECT_BUS);
+  }
 }
 
 /*!
@@ -352,16 +382,21 @@ geda_bus_object_new(int color, int x1, int y1, int x2, int y2, int dir)
  * \return The orientation: HORIZONTAL, VERTICAL or NEITHER
  */
 int
-geda_bus_object_orientation(GedaObject *object)
+geda_bus_object_orientation(const GedaObject *object)
 {
-  if (object->line->y[0] == object->line->y[1]) {
-    return(HORIZONTAL);
-  }
+  if (GEDA_IS_BUS(object)) {
 
-  if (object->line->x[0] == object->line->x[1]) {
-    return(VERTICAL);
-  }
+    if (object->line->y[0] == object->line->y[1]) {
+      return(HORIZONTAL);
+    }
 
+    if (object->line->x[0] == object->line->x[1]) {
+      return(VERTICAL);
+    }
+  }
+  else {
+    geda_object_error(__func__, object, GEDA_OBJECT_BUS);
+  }
   return(NEITHER);
 }
 
@@ -382,31 +417,37 @@ geda_bus_object_print(GedaToplevel *toplevel,  FILE *fp,
                       GedaObject   *o_current, int   origin_x,
                                                int   origin_y)
 {
-  int bus_width;
-  int x1, y1;
-  int x2, y2;
+  if (GEDA_IS_BUS(o_current)) {
 
-  if (o_current == NULL) {
-    printf (_("null in geda_bus_object_print\n"));
-    return;
+    int bus_width;
+    int x1, y1;
+    int x2, y2;
+
+    if (o_current == NULL) {
+      printf (_("null in geda_bus_object_print\n"));
+      return;
+    }
+
+    f_print_set_color(toplevel, fp, o_current->color);
+
+    bus_width = o_current->line_options->line_width;
+    if(bus_width <= MIN_LINE_WIDTH_THRESHOLD)
+      bus_width = o_style_get_bus_width(toplevel);  /* 1st try updating the style */
+      if (bus_width < MIN_LINE_WIDTH_THRESHOLD) /* if STYLE_NONE */
+        bus_width = MIN_LINE_WIDTH_THRESHOLD;
+
+      x1 = o_current->line->x[0]-origin_x,
+      y1 = o_current->line->y[0]-origin_y;
+    x2 = o_current->line->x[1]-origin_x,
+    y2 = o_current->line->y[1]-origin_y;
+
+    fprintf(fp, "%d %d %d %d %d %d bus\n", x1,y1,x2,y2,bus_width,SQUARE_CAP);
   }
-
-  f_print_set_color(toplevel, fp, o_current->color);
-
-  bus_width = o_current->line_options->line_width;
-  if(bus_width <= MIN_LINE_WIDTH_THRESHOLD)
-    bus_width = o_style_get_bus_width(toplevel);  /* 1st try updating the style */
-  if (bus_width < MIN_LINE_WIDTH_THRESHOLD) /* if STYLE_NONE */
-    bus_width = MIN_LINE_WIDTH_THRESHOLD;
-
-  x1 = o_current->line->x[0]-origin_x,
-  y1 = o_current->line->y[0]-origin_y;
-  x2 = o_current->line->x[1]-origin_x,
-  y2 = o_current->line->y[1]-origin_y;
-
-  fprintf(fp, "%d %d %d %d %d %d bus\n", x1,y1,x2,y2,bus_width,SQUARE_CAP);
-
+  else {
+    geda_object_error(__func__, o_current, GEDA_OBJECT_BUS);
+  }
 }
+
 /*!
  * \brief read a bus object from a char buffer
  * \par Function Description
@@ -490,39 +531,50 @@ geda_bus_object_read(const char     buf[],
 void
 geda_bus_object_rotate(GedaObject *object, int center_x, int center_y, int angle)
 {
-  int newx, newy;
+  if (GEDA_IS_BUS(object)) {
 
-  if (angle == 0)
-  return;
+    int newx, newy;
 
-  /* translate object to origin */
-  geda_bus_object_translate(object, -center_x, -center_y);
+    if (angle == 0)
+      return;
 
-  m_rotate_point_90(object->line->x[0], object->line->y[0], angle,
-                  &newx, &newy);
+    /* translate object to origin */
+    geda_bus_object_translate(object, -center_x, -center_y);
 
-  object->line->x[0] = newx;
-  object->line->y[0] = newy;
+    m_rotate_point_90(object->line->x[0], object->line->y[0], angle,
+                      &newx, &newy);
 
-  m_rotate_point_90(object->line->x[1], object->line->y[1], angle,
-                  &newx, &newy);
+    object->line->x[0] = newx;
+    object->line->y[0] = newy;
 
-  object->line->x[1] = newx;
-  object->line->y[1] = newy;
+    m_rotate_point_90(object->line->x[1], object->line->y[1], angle,
+                      &newx, &newy);
 
-  geda_bus_object_translate(object, center_x, center_y);
+    object->line->x[1] = newx;
+    object->line->y[1] = newy;
+
+    geda_bus_object_translate(object, center_x, center_y);
+  }
+  else {
+    geda_object_error(__func__, object, GEDA_OBJECT_BUS);
+  }
 }
 
-/*! \brief Set the Bus ripper direction
+/*!
+ * \brief Set the Bus ripper direction
+ * \par Function Description
  *
- *  \param [in,out] object The bus object
- *  \param [in] direction The ripper direction
+ * \param [in,out] object    The bus object
+ * \param [in]     direction The ripper direction
  */
 void
-geda_bus_object_set_ripper_direction (GedaObject *object, gint direction)
+geda_bus_object_set_ripper_direction (GedaObject *object, int direction)
 {
   if (GEDA_IS_BUS(object)) {
     object->bus->bus_ripper_direction = direction;
+  }
+  else {
+    geda_object_error(__func__, object, GEDA_OBJECT_BUS);
   }
 }
 
@@ -542,18 +594,22 @@ geda_bus_object_set_ripper_direction (GedaObject *object, gint direction)
 char*
 geda_bus_object_to_buffer(GedaObject *object)
 {
-  int x1, x2, y1, y2;
-  char *buf;
+  if (GEDA_IS_BUS(object)) {
+    int x1, x2, y1, y2;
+    char *buf;
 
-  x1 = object->line->x[0];
-  y1 = object->line->y[0];
-  x2 = object->line->x[1];
-  y2 = object->line->y[1];
+    x1 = object->line->x[0];
+    y1 = object->line->y[0];
+    x2 = object->line->x[1];
+    y2 = object->line->y[1];
 
-  buf = geda_sprintf("%c %d %d %d %d %d %d", object->type,
-                         x1, y1, x2, y2, object->color,
-                         object->bus->bus_ripper_direction);
-  return(buf);
+    buf = geda_sprintf("%c %d %d %d %d %d %d", object->type,
+    x1, y1, x2, y2, object->color,
+    object->bus->bus_ripper_direction);
+    return(buf);
+  }
+  geda_object_error(__func__, object, GEDA_OBJECT_BUS);
+  return NULL;
 }
 
 /*!
@@ -568,15 +624,20 @@ geda_bus_object_to_buffer(GedaObject *object)
 void
 geda_bus_object_translate(GedaObject *object, int dx, int dy)
 {
-  /* Update world coords */
-  object->line->x[0] = object->line->x[0] + dx;
-  object->line->y[0] = object->line->y[0] + dy;
-  object->line->x[1] = object->line->x[1] + dx;
-  object->line->y[1] = object->line->y[1] + dy;
+  if (GEDA_IS_BUS(object)) {
 
-  /* Update bounding box */
-  object->w_bounds_valid_for = NULL;
+    /* Update world coords */
+    object->line->x[0] = object->line->x[0] + dx;
+    object->line->y[0] = object->line->y[0] + dy;
+    object->line->x[1] = object->line->x[1] + dx;
+    object->line->y[1] = object->line->y[1] + dy;
 
-  s_tile_update_object(object);
+    /* Update bounding box */
+    object->w_bounds_valid_for = NULL;
 
+    s_tile_update_object(object);
+  }
+  else {
+    geda_object_error(__func__, object, GEDA_OBJECT_BUS);
+  }
 }
