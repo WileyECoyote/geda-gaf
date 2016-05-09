5574696c6271a51caf635b701746ebd9d2042604
WileyECoyote <wileyhill@gmail.com>
2016-05-09 00:47:07 -0500

    libgedauio/tests: add checks for _combo_text_get_entry

--- libgedauio/tests/test_combobox_text.c
+++ libgedauio/tests/test_combobox_text.c
@@ -39,6 +39,11 @@
 #include <../include/geda_comboboxtext.h>
 #include <../include/geda_bulb.h>
 
+#include "test-suite.h"
+
+/*! \def MUT Module Under Tests */
+#define MUT "src/widgets/geda_combobox_text.c"
+
 #define TWIDGET "GedaComboBoxText"
 
 /*! \file test_combobox_text.c
@@ -119,10 +124,40 @@ int check_construction (void)
 }
 
 int
+check_accessors ()
+{
+  int result = 0;
+
+  GtkWidget *widget = geda_combo_box_text_new_with_entry();
+
+  if (!GEDA_IS_COMBO_BOX_TEXT(widget)) {
+    fprintf(stderr, "FAILED: line <%d> is a %s\n", __LINE__, TWIDGET);
+    result++;
+  }
+  else {
+
+    GedaComboBoxText *combo_text = GEDA_COMBO_BOX_TEXT(widget);
+    GtkWidget *entry;
+
+    /* Uses geda_combo_get_entry_widget */
+    entry = geda_combo_box_text_get_entry_widget(combo_text);
+
+    if (!GEDA_IS_ENTRY(entry)) {
+      fprintf(stderr, "FAILED: line <%d> _get_entry\n", __LINE__);
+      result++;
+    }
+  }
+
+  g_object_unref(widget);    /* Destroy the widget */
+  return result;
+}
+
+int
 main (int argc, char *argv[])
 {
   int result = 0;
-  int subtotal = 0;
+
+  SETUP_SIGSEGV_HANDLER;
 
   /* Initialize gobject */
 #if (( GLIB_MAJOR_VERSION == 2 ) && ( GLIB_MINOR_VERSION < 36 ))
@@ -131,11 +166,23 @@ main (int argc, char *argv[])
 
   if (gtk_init_check(&argc, &argv)) {
 
-    subtotal = check_construction();
-    if (subtotal) {
-      fprintf(stderr, "Check %s constructors", TWIDGET);
-      result   = subtotal;
-      subtotal = 0;
+    if (setjmp(point) == 0) {
+      result = check_construction();
+    }
+    else {
+      fprintf(stderr, "Caught signal checking constructors in %s\n\n", MUT);
+      return 1;
+    }
+
+    if (!result) {
+
+      if (setjmp(point) == 0) {
+        result = check_accessors();
+      }
+      else {
+        fprintf(stderr, "Caught signal checking accessors in %s\n\n", MUT);
+        return 1;
+      }
     }
   }
   return result;
