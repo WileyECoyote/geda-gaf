9b5966844226b065c421d0402e2b6dd898d191af
WileyECoyote <wileyhill@gmail.com>
2016-05-27 09:53:42 -0500

    libgedauio: check construction of GedaToolBar widgets

---
+++ libgedauio/tests/test_toolbar.c
@@ -0,0 +1,101 @@
+/* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 2 tab-width: 2 -*- */
+/* vi: set et ts=2 sw=2 sts=2: */
+/*
+ * File: test_toolbar.c
+ *
+ * gEDA - GPL Electronic Design Automation
+ * libgedauio - gEDA's library for User Interface Objects
+ *
+ * Copyright (C) 2016 gEDA Contributors (see ChangeLog for details)
+ *
+ * This Library is free software; you can redistribute it and or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; version 3 of the
+ * License.
+ *
+ * This Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA <http://www.gnu.org/licenses/>.
+ *
+ * Date: May, 27, 2016
+ * Contributing Author: Wiley Edward Hill
+ */
+
+#include "config.h"
+
+#include <string.h>
+
+#include <glib.h>
+#include <gtk/gtk.h>
+
+#include <geda/geda.h>
+#include <geda_toolbar.h>
+
+#include "test-suite.h"
+
+/*! \def MUT Module Under Tests */
+#define MUT "src/widgets/geda_toolbar.c"
+
+#define TWIDGET "GedaToolbar"
+
+/*! \file test_toolbar.c
+ *  \brief Tests for geda_toolbar.c module
+ */
+
+int check_construction (void)
+{
+  int result = 0;
+
+  GtkWidget *widget = geda_toolbar_new(0);
+
+  if (!GEDA_IS_TOOLBAR(widget)) {
+    fprintf(stderr, "FAILED: line <%d> is a %s\n", __LINE__, TWIDGET);
+    result++;
+  }
+
+  if (!GTK_IS_TOOLBAR(widget)) {
+    fprintf(stderr, "FAILED: line <%d> is a %s\n", __LINE__, TWIDGET);
+    result++;
+  }
+
+  if (!GTK_IS_CONTAINER(widget)) {
+    fprintf(stderr, "FAILED: line <%d> is a %s\n", __LINE__, TWIDGET);
+    result++;
+  }
+
+  g_object_ref_sink(widget); /* Sink reference to menu_item */
+  g_object_unref(widget);    /* Does not destroy widget */
+
+  return result;
+}
+
+int
+main (int argc, char *argv[])
+{
+  int result = 0;
+
+  SETUP_SIGSEGV_HANDLER;
+
+  /* Initialize gobject */
+#if (( GLIB_MAJOR_VERSION == 2 ) && ( GLIB_MINOR_VERSION < 36 ))
+  g_type_init();
+#endif
+
+  if (gtk_init_check(&argc, &argv)) {
+
+    if (setjmp(point) == 0) {
+      result = check_construction();
+    }
+    else {
+      fprintf(stderr, "Caught signal checking constructors in %s\n\n", MUT);
+    }
+  }
+
+  return result;
+}
