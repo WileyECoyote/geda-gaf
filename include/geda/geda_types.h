33342a10b48dedaf71daab7eea0d0f90b4715e1e
WileyECoyote <wileyhill@gmail.com>
2016-07-02 13:47:24 -0500

    add miscellaneous int_t and unint_t types

--- include/geda/geda_types.h
+++ include/geda/geda_types.h
@@ -50,8 +50,15 @@ typedef enum { FALSE, TRUE } Boolean;
 typedef unsigned char uint8;
 typedef unsigned long DWORD;
 
+typedef int8_t   int8;
+typedef int16_t  int16;
+typedef int32_t  int32;
+typedef int64_t  int64;
+
+typedef uint8_t  uint8;
 typedef uint16_t uint16;
 typedef uint32_t uint32;
+typedef uint64_t uint64;
 
 #if defined(__LP64__) || defined(_LP64)
 # define IS64BIT
