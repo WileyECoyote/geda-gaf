bb25af5417c91665d79dac043319d12ee7e08354
WileyECoyote <wileyhill@gmail.com>
2016-07-30 03:00:58 -0500

    libgedauio/tests: expand tests for GedaMenuShell

--- libgedauio/tests/test_menu_shell.c
+++ libgedauio/tests/test_menu_shell.c
@@ -36,7 +36,14 @@
 
 #include <geda/geda.h>
 #include <geda_menu.h>
+#include <geda_menu_bar.h>
+#include <geda_menu_item.h>
 #include <geda_menu_shell.h>
+
+#include "test-suite.h"
+
+/*! \def MUT Module Under Tests */
+#define MUT "src/widgets/geda_menu_shell.c"
 
 #define TWIDGET "GedaMenuShell"
 
@@ -71,11 +78,102 @@ int check_construction (void)
   return result;
 }
 
+static GtkWidget *main_window()
+{
+  GtkWidget *vbox;
+  GtkWidget *window;
+  GtkWidget *menubar;
+
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+
+  vbox = gtk_vbox_new (FALSE, 0);
+  gtk_container_add (GTK_CONTAINER (window), vbox);
+  gtk_widget_show (vbox);
+
+  menubar = geda_menu_bar_new ();
+  gtk_box_pack_start (GTK_BOX (vbox), menubar, FALSE, TRUE, 0);
+
+  gtk_widget_show (menubar);
+  gtk_widget_show (window);
+
+  return menubar;
+}
+
+int
+check_accessors ()
+{
+  int result = 0;
+
+  GtkWidget     *menu;
+  GtkWidget     *menu_bar;
+  GtkWidget     *widget0;
+  GtkWidget     *widget1;
+  GtkWidget     *widget2;
+  GtkWidget     *widget3;
+  GedaMenuItem  *menu_item;
+  GedaMenuShell *menu_shell;
+
+  widget0 = geda_menu_item_new_with_mnemonic("_Fruit");
+
+  menu       = geda_menu_new ();
+  menu_bar   = main_window();
+  menu_item  = GEDA_MENU_ITEM(widget0);
+  menu_shell = GEDA_MENU_SHELL (menu);
+
+  geda_menu_item_set_submenu (GEDA_MENU_ITEM (menu_item), menu);
+  geda_menu_shell_append (GEDA_MENU_SHELL (menu_bar), widget0);
+
+  widget1    = geda_menu_item_new_with_mnemonic("_Cherry");
+  gtk_container_add (GTK_CONTAINER (menu_shell), widget1);
+  gtk_widget_show (widget1);
+
+  widget2    = geda_menu_item_new_with_mnemonic("_Apple");
+  geda_menu_shell_append (menu_shell, widget2);
+  gtk_widget_show (widget2);
+
+  widget3    = geda_menu_item_new_with_mnemonic("_Pears");
+  geda_menu_shell_append (menu_shell, widget3);
+  gtk_widget_show (widget3);
+
+  gtk_widget_show (menu);
+
+  int value;
+
+  /* -------------------- take_focus -------------------- */
+
+  value = geda_menu_shell_get_take_focus (menu_shell);
+  if (!value) {  /* take_focus default is TRUE */
+    fprintf(stderr, "FAILED: line <%d> take_focus %d\n", __LINE__, value);
+    result++;
+  }
+
+  geda_menu_shell_set_take_focus (menu_shell, FALSE);
+
+  value = geda_menu_shell_get_take_focus (menu_shell);
+  if (value) {
+    fprintf(stderr, "FAILED: line <%d> get/set take_focus %s\n", __LINE__, TWIDGET);
+    result++;
+  }
+
+  geda_menu_shell_set_take_focus (menu_shell, TRUE);
+
+  value = geda_menu_shell_get_take_focus (menu_shell);
+  if (!value) {
+    fprintf(stderr, "FAILED: line <%d> set/get take_focus %s\n", __LINE__, TWIDGET);
+    result++;
+  }
+
+  gtk_widget_destroy(gtk_widget_get_toplevel(widget0));
+
+  return result;
+}
+
 int
 main (int argc, char *argv[])
 {
   int result = 0;
-  int subtotal = 0;
+
+  SETUP_SIGSEGV_HANDLER;
 
   /* Initialize gobject */
 #if (( GLIB_MAJOR_VERSION == 2 ) && ( GLIB_MINOR_VERSION < 36 ))
@@ -84,12 +182,22 @@ main (int argc, char *argv[])
 
   if (gtk_init_check(&argc, &argv)) {
 
-    subtotal = check_construction();
+    if (setjmp(point) == 0) {
+      result = check_construction();
+    }
+    else {
+      fprintf(stderr, "Caught signal checking constructors in %s\n\n", MUT);
+    }
 
-    if (subtotal) {
-      fprintf(stderr, "Check constructors in src/widgets/geda_menu_shell.c");
-      result   = subtotal;
-      subtotal = 0;
+    if (!result) {
+
+      if (setjmp(point) == 0) {
+        result = check_accessors();
+      }
+      else {
+        fprintf(stderr, "Caught signal checking accessors in %s\n\n", MUT);
+        return 1;
+      }
     }
   }
   return result;
