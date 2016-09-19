ebc70d8edc75bf1ece0f650fbf30108b46e77582
WileyECoyote <wileyhill@gmail.com>
2016-09-18 20:34:28 -0500

    libgeda: Add macro file for math modules

---
+++ libgeda/include/libgeda/geda_math.h
@@ -0,0 +1,42 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4 tab-width: 4 -*- */
+/*
+ * File: geda_math.h
+ *
+ * gEDA - GPL Electronic Design Automation
+ * libgeda - gEDA's library
+ *
+ * Copyright (C) 2016 Wiley Edward Hill
+ * Copyright (C) 2016 gEDA Contributors (see ChangeLog for details)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA <http://www.gnu.org/licenses/>.
+ *
+ *  Contributing Author: Wiley Edward Hill
+ *  Date Contributed: Sept, 18, 2016
+ */
+/*! \file geda_math.h "libgeda/geda_math.h"
+ *  \brief Libgeda macros for math modules.
+ */
+
+#ifndef __GEDA_MATH_MAC__
+#define __GEDA_MATH_MAC__
+
+/* m_polygon.c */
+#define geda_polygon_interior_point     geda_math_polygon_interior_point
+#define geda_polygon_shortest_distance  geda_math_polygon_shortest_distance
+#define geda_polygon_append_bezier      geda_math_polygon_append_bezier
+#define geda_polygon_append_point       geda_math_polygon_append_point
+
+#endif /* __GEDA_MATH_MAC__ */
