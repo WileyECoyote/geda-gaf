481cef1fa197847c33ac2f689ffe35e922fd4189
WileyECoyote <wileyhill@gmail.com>
2016-10-19 15:28:13 -0500

    gaf: update command-line copyright

--- gaf/src/gaf.c
+++ gaf/src/gaf.c
@@ -87,7 +87,7 @@ version (void)
 {
 
   printf(_("gEDA/gaf %s (%s) (g%.7s)\n"
-  "Copyright (C) 1998-2013 gEDA developers\n"
+  "Copyright (C) 1998-2016 gEDA developers\n"
   "This is free software, and you are welcome to redistribute it under\n"
   "certain conditions. For details, see the file `COPYING', which is\n"
   "included in the gEDA distribution.\n"
