62eb4d345967a6eb5829c84d521800645ced11eb
WileyECoyote <wileyhill@gmail.com>
2016-11-01 02:20:23 -0500

    modify conditions for definition of DIR_SEPARATOR

--- include/geda/geda_system.h
+++ include/geda/geda_system.h
@@ -28,8 +28,15 @@
  * @{\par This group contains system related defines
  *   \ingroup (geda-globals)
  */
+#if defined(__linux__) || defined(UNIX) || defined(__MINGW32__)
 
-#if defined(WIN32) || defined(_WIN32)
+#define DIR_SEPARATOR 0x2F
+#define DIR_SEPARATOR_S "/"
+#define IS_DIR_SEPARATOR(c) ((c) == DIR_SEPARATOR)
+#define SEARCHPATH_SEPARATOR ':'
+#define SEARCHPATH_SEPARATOR_S ":"
+
+#elif defined(OS_WIN32_NATIVE) || defined(_WIN32)
 
 /* On Win32, the directory separator is the backslash, and the search path
  * separator is the semicolon. Note that also the (forward) slash works as
@@ -40,14 +47,6 @@
 #define IS_DIR_SEPARATOR(c) ((c) == DIR_SEPARATOR || (c) == '/')
 #define SEARCHPATH_SEPARATOR ';'
 #define SEARCHPATH_SEPARATOR_S ";"
-
-#elif defined(__linux__) || defined(UNIX)
-
-#define DIR_SEPARATOR 0x2F
-#define DIR_SEPARATOR_S "/"
-#define IS_DIR_SEPARATOR(c) ((c) == DIR_SEPARATOR)
-#define SEARCHPATH_SEPARATOR ':'
-#define SEARCHPATH_SEPARATOR_S ":"
 
 #else
 #error "No suitable OS defined"
