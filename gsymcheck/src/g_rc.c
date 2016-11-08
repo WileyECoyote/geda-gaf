1f81dc566b7dd85e76495d87491f96c41410de77
WileyECoyote <wileyhill@gmail.com>
2016-11-08 00:20:16 -0600

    gsymcheck: substitute macro geda_gslist_free_all

--- gsymcheck/src/g_rc.c
+++ gsymcheck/src/g_rc.c
@@ -82,7 +82,7 @@ SCM g_rc_valid_attributes(SCM stringlist)
   g_list_free(default_valid_attributes);
 
   scm_dynwind_begin(0);
-  scm_dynwind_unwind_handler(geda_utility_gslist_free_all, (void *) &list, 0);
+  scm_dynwind_unwind_handler(geda_gslist_free_all, (void*)&list, 0);
 
   /* convert the scm list into a GList */
   for (i=0; i < length; i++) {
