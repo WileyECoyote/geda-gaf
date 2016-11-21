59767c1d0306dfc33c5c55353ee9dbe5409d7ecd
WileyECoyote <wileyhill@gmail.com>
2016-11-21 00:49:28 -0600

    libgeda: update Doxygen file name

--- libgeda/src/math/m_angle.c
+++ libgeda/src/math/m_angle.c
@@ -17,9 +17,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
-/*! \file geda_math_angle.c
- *
- *  \brief Functions for working with angles
+/*!
+ * \file m_angle.c
+ * \brief Functions for working with angles
  */
 
 #include <config.h>
