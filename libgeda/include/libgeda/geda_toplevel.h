ac821b8b234aa9a90d1b858289395609ab0bbe96
WileyECoyote <wileyhill@gmail.com>
2016-12-02 14:08:47 -0600

    libgeda: add missing cast in GEDA_IS_TOPLEVEL macro

--- libgeda/include/libgeda/geda_toplevel.h
+++ libgeda/include/libgeda/geda_toplevel.h
@@ -49,7 +49,7 @@
 #define GEDA_TYPE_TOPLEVEL            (geda_toplevel_get_type())
 #define GEDA_TOPLEVEL(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj), GEDA_TYPE_TOPLEVEL, GedaToplevel))
 #define GEDA_TOPLEVEL_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass),  GEDA_TYPE_TOPLEVEL, GedaToplevelClass))
-#define GEDA_IS_TOPLEVEL(obj)         (is_a_geda_toplevel(obj))
+#define GEDA_IS_TOPLEVEL(obj)         (is_a_geda_toplevel((GedaToplevel*)(obj)))
 #define GEDA_IS_TOPLEVEL_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass),  GEDA_TYPE_TOPLEVEL))
 #define GEDA_TOPLEVEL_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj),  GEDA_TYPE_TOPLEVEL, GedaToplevelClass))
 
