caf54ed743506ef90c51894fb59911ddc0e9f61a
WileyECoyote <wileyhill@gmail.com>
2017-05-03 01:52:41 -0500

    libgedauio: add macro geda_container_add

--- libgedauio/include/geda_container.h
+++ libgedauio/include/geda_container.h
@@ -10,6 +10,8 @@ geda_container_focus_sort (GtkContainer     *container,
                            GtkDirectionType  direction,
                            GtkWidget        *old_focus);
 
+#define geda_container_add(c, w) gtk_container_add((GtkContainer*)c, (GtkWidget*)w)
+
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
