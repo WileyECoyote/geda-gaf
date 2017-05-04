7c920ecc9154925b59947fc972ebe916453deb82
WileyECoyote <wileyhill@gmail.com>
2017-05-04 02:30:03 -0500

    libgedauio: add more container macros

--- libgedauio/include/geda_container.h
+++ libgedauio/include/geda_container.h
@@ -11,6 +11,15 @@ geda_container_focus_sort (GtkContainer     *container,
                            GtkWidget        *old_focus);
 
 #define geda_container_add(c, w) gtk_container_add((GtkContainer*)c, (GtkWidget*)w)
+#define geda_container_remove(c, w) gtk_container_remove((GtkContainer*)c, (GtkWidget*)w)
+
+#define geda_container_get_children(c) gtk_container_get_children((GtkContainer*)c)
+
+#define geda_set_container_border_width(c, w) gtk_container_set_border_width((GtkContainer*)c, w)
+#define geda_container_set_focus_chain(c, fc) gtk_container_set_focus_chain ((GtkContainer*)c, fc);
+
+#define geda_container_forall(c, f, d )gtk_container_forall ((GtkContainer*)c, (GtkCallback)f, (void*)d)
+#define geda_container_foreach(c, f, d )gtk_container_forall ((GtkContainer*)c, (GtkCallback)f, (void*)d)
 
 #ifdef __cplusplus
 }
