998814910d57e3653abb93822f24e1d748567ab2
WileyECoyote <wileyhill@gmail.com>
2017-10-19 02:31:05 -0500

    gnetlist.scm: remove extra space in message

--- gnetlist/scheme/gnetlist.scm
+++ gnetlist/scheme/gnetlist.scm
@@ -340,7 +340,7 @@ of REFDES. As a result, slots may be repeated in the returned list."
       string-to-wrap ; Last snippet of string
       (let ((pos (string-rindex string-to-wrap #\space 0 wrap-length)))
 	(cond ((not pos)
-	       (display "Couldn't wrap string  at requested position\n")
+	       (display "Could not wrap string at requested position\n")
 	       " Wrap error!")
 	      (else
 	       (string-append
