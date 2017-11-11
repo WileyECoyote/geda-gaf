d7af2f7111ddc8c6e10fb46595b6eca7663f5185
WileyECoyote <wileyhill@gmail.com>
2017-11-11 13:14:21 -0600

    libgeda/tests: checks for path line/fill properties

--- libgeda/tests/test_path.c
+++ libgeda/tests/test_path.c
@@ -129,6 +129,148 @@ int test_path (void)
 }
 
 int
+check_properties (void)
+{
+  int result = 0;
+
+  GedaObject *object = geda_path_new();
+
+  if (!GEDA_IS_PATH(object->path)) {
+    fprintf(stderr, "is a %s Failed line <%d>\n", TOBJECT, __LINE__);
+    result++;
+  }
+  else {
+
+    GedaPath *path = object->path;
+
+    int count;
+    int fail;
+
+    fail = 0;
+
+    for (count = 0; count < 10; count++) {
+
+      /* Line type options */
+      int e = geda_random_number (END_NONE, END_ROUND);
+      int t = geda_random_number (TYPE_SOLID, TYPE_PHANTOM);
+      int l = geda_random_number (5, 500);
+      int p = geda_random_number (5, 500);
+      int w = geda_random_number (0, 500);
+
+      /* Filling options */
+      int ft  = geda_random_number (FILLING_HOLLOW, FILLING_HATCH);
+      int fw  = geda_random_number (0, 100);
+      int fa1 = geda_random_number (0, 180);
+      int fp1 = geda_random_number (0, 500);
+      int fa2 = geda_random_number (0, 180);
+      int fp2 = geda_random_number (0, 500);
+
+      /* Check line type properties */
+      g_object_set(path, "end-cap",     e,
+                         "line-type",   t,
+                         "line-width",  w,
+                         "line-space",  p,
+                         "line-length", l,
+                          NULL);
+
+      int re, rt, rw, rp, rl;
+
+      g_object_get(path, "end-cap",     &re,
+                         "line-type",   &rt,
+                         "line-width",  &rw,
+                         "line-space",  &rp,
+                         "line-length", &rl,
+                          NULL);
+
+      if (e - re) {
+        fprintf(stderr, "FAILED: %s get/set end-cap property <%d>\n", TOBJECT, re);
+        fail++;
+      }
+
+      if (t - rt) {
+        fprintf(stderr, "FAILED: %s get/set line-type property <%d>\n", TOBJECT, rt);
+        fail++;
+      }
+
+      if (w - rw) {
+        fprintf(stderr, "FAILED: %s get/set line-width property <%d>\n", TOBJECT, rw);
+        fail++;
+      }
+
+      if (p - rp) {
+        fprintf(stderr, "FAILED: %s get/set line-space property <%d>\n", TOBJECT, rp);
+        fail++;
+      }
+
+      if (l - rl) {
+        fprintf(stderr, "FAILED: %s get/set line-length property <%d>\n", TOBJECT, rl);
+        fail++;
+      }
+
+      /* Check Filling properties */
+      g_object_set(path, "fill-type",   ft,
+                         "fill-width",  fw,
+                         "fill-angle1", fa1,
+                         "fill-pitch1", fp1,
+                         "fill-angle2", fa2,
+                         "fill-pitch2", fp2,
+                          NULL);
+
+      int rft, rfw, rfa1, rfp1, rfa2, rfp2;
+
+      g_object_get(path, "fill-type",   &rft,
+                         "fill-width",  &rfw,
+                         "fill-angle1", &rfa1,
+                         "fill-pitch1", &rfp1,
+                         "fill-angle2", &rfa2,
+                         "fill-pitch2", &rfp2,
+                          NULL);
+
+      if (ft - rft) {
+        fprintf(stderr, "FAILED: %s get/set fill-type property <%d>\n", TOBJECT, rft);
+        fail++;
+      }
+
+      if (fw - rfw) {
+        fprintf(stderr, "FAILED: %s get/set fill-width property <%d>\n", TOBJECT, rfw);
+        fail++;
+      }
+
+      if (fa1 - rfa1) {
+        fprintf(stderr, "FAILED: %s get/set fill-angle1 property <%d>\n", TOBJECT, rfa1);
+        fail++;
+      }
+
+      if (fp1 - rfp1) {
+        fprintf(stderr, "FAILED: %s get/set fill-pitch1 property <%d>\n", TOBJECT, fp1);
+        fail++;
+      }
+
+      if (fa2 - rfa2) {
+        fprintf(stderr, "FAILED: %s get/set fill-angle2 property <%d>\n", TOBJECT, rfa2);
+        fail++;
+      }
+
+      if (fp2 - rfp2) {
+        fprintf(stderr, "FAILED: %s get/set fill-pitch1 property <%d>\n", TOBJECT, fp2);
+        fail++;
+      }
+
+      if (fail) {
+
+        fprintf(stderr, "FAILED: to get or set %d %s propert%s\n", fail, TOBJECT,
+                fail > 1 ? "ies" : "y");
+
+        result = fail;
+        break;
+      }
+    }
+  }
+
+  return result;
+}
+
+int
 main (int argc, char *argv[])
 {
   int result = 0;
@@ -139,6 +281,7 @@ main (int argc, char *argv[])
 #endif
 
   result = test_path();
+  result += check_properties();
 
   return result;
 }
