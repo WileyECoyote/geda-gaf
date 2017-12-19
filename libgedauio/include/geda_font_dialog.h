16641fc361c3ecf1ee49cbf28aef88d515bf859b
WileyECoyote <wileyhill@gmail.com>
2017-12-19 11:08:07 -0600

    libgedauio: add macro geda_font_dialog_get_title

--- libgedauio/include/geda_font_dialog.h
+++ libgedauio/include/geda_font_dialog.h
@@ -153,6 +153,7 @@ void        geda_font_dialog_set_show_preview   (GedaFontDialog *dialog,
                                                  bool            show_preview);
 
 #define geda_font_dialog_set_title(d, t) gtk_window_set_title ((GtkWindow*)d, t);
+#define geda_font_dialog_get_title(d)    gtk_window_get_title ((GtkWindow*)d)
 
 #ifdef __cplusplus
 }
