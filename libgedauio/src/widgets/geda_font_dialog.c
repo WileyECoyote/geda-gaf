53250e421fa39a73a6c091d1daf706f01abd8a16
WileyECoyote <wileyhill@gmail.com>
2018-01-09 00:50:50 -0600

    geda_font_dialog.c: utilize GEDA_OBJECT_NOTIFY macro

--- libgedauio/src/widgets/geda_font_dialog.c
+++ libgedauio/src/widgets/geda_font_dialog.c
@@ -178,7 +178,7 @@ geda_font_dialog_update_preview (GedaFontDialog *dialog)
 static void
 callback_update_preview (GtkWidget *entry, GedaFontDialog *dialog)
 {
-  g_object_notify ((GObject*)dialog, "preview-text");
+  GEDA_OBJECT_NOTIFY (dialog, "preview-text");
 }
 
 static void
