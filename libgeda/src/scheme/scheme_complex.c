4c14ed2f354b29f6b255f3e77c696bba9e34fc90
WileyECoyote <wileyhill@gmail.com>
2018-01-15 12:15:03 -0600

    scheme_complex.c: remove redundant setting of w_bounds_valid_for

--- libgeda/src/scheme/scheme_complex.c
+++ libgeda/src/scheme/scheme_complex.c
@@ -174,13 +174,12 @@ EDA_SCM_DEFINE (complex_set_x, "%set-complex!", 6, 0, 0,
   if (angle < 0) {
     angle += 360;
   }
+
   geda_object_rotate (obj, x, y, angle);
 
   geda_object_translate (obj, x - obj->complex->x, y - obj->complex->y);
 
   obj->selectable = scm_is_false (locked_s);
-
-  obj->w_bounds_valid_for = NULL; /* We need to do this explicitly... */
 
   geda_object_notify_emit_change (obj);
 
