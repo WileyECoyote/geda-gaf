b0b8727611ffc5d6f6adda7ca2fcbd89a5d2c26b
WileyECoyote <wileyhill@gmail.com>
2018-01-16 01:06:31 -0600

    geda_image_chooser.c: use geda_marshal_VOID__ENUM

--- libgedauio/src/dialogs/geda_image_chooser.c
+++ libgedauio/src/dialogs/geda_image_chooser.c
@@ -1058,7 +1058,7 @@ geda_image_chooser_class_init (void *class, void *data)
                                                                        geometry_restore ),
                                                       NULL, /* accumulator */
                                                       NULL, /* accu_data */
-                                                      g_cclosure_marshal_VOID__STRING,
+                                                      geda_marshal_VOID__STRING,
                                                       G_TYPE_NONE,
                                                       1,    /* n_params */
                                                       G_TYPE_STRING);
@@ -1069,7 +1069,7 @@ geda_image_chooser_class_init (void *class, void *data)
                                                                        geometry_save ),
                                                       NULL, /* accumulator */
                                                       NULL, /* accu_data */
-                                                      g_cclosure_marshal_VOID__STRING,
+                                                      geda_marshal_VOID__STRING,
                                                       G_TYPE_NONE,
                                                       1,    /* n_params */
                                                       G_TYPE_STRING);
