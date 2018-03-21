8c682348cffbe0ed6dd01a5c18285b391a2baf8d
WileyECoyote <wileyhill@gmail.com>
2018-03-21 00:21:38 -0500

    geda_object.h: add function geda_get_object_type inline

--- libgeda/include/libgeda/geda_object.h
+++ libgeda/include/libgeda/geda_object.h
@@ -154,6 +154,11 @@ void          geda_object_set_color          (GedaObject *object, int color);
 void          geda_object_set_selectable     (GedaObject *object, int state);
 void          geda_object_set_locked_color   (GedaObject *object, int color);
 
+inline int    geda_get_object_type           (GedaObject *object)
+{
+    return object->type;
+};
+
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
