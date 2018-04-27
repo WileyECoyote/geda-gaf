80381f2d34a9ce522b94552d88694a9bb931ba27
WileyECoyote <wileyhill@gmail.com>
2018-04-27 12:07:03 -0500

    i_command.c: revise add line event handling

--- gschem/src/iface/i_command.c
+++ gschem/src/iface/i_command.c
@@ -3384,17 +3384,13 @@ COMMAND (do_add_line)
   o_redraw_cleanstates(w_current);
   o_invalidate_rubber (w_current);
 
-  if (w_current->action_event->state) {
-    i_event_cancel_action_handler(w_current);
-  }
-
   i_status_set_state(w_current, LINEMODE);
 
   if HOT_ACTION (do_add_line) {
     o_line_start (w_current, CMD_X(do_add_line), CMD_Y(do_add_line));
   }
   else {
-    i_status_action_stop(w_current);
+    i_event_end_action_handler(w_current);
   }
 
   EXIT_COMMAND(do_add_line);
