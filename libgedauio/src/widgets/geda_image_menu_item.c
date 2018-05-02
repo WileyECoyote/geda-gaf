b6b226c17fd77e7e9a56c2378018fa5c347ebf42
WileyECoyote <wileyhill@gmail.com>
2018-05-02 00:31:38 -0500

    geda_image_menu_item.c: unconditionally validate item

--- libgedauio/src/widgets/geda_image_menu_item.c
+++ libgedauio/src/widgets/geda_image_menu_item.c
@@ -1075,13 +1075,13 @@ bool geda_image_menu_item_get_show_image (GedaImageMenuItem *image_menu_item)
 void geda_image_menu_item_set_accel_group (GedaImageMenuItem *image_menu_item,
                                            GtkAccelGroup     *accel_group)
 {
+  g_return_if_fail (GEDA_IS_IMAGE_MENU_ITEM (image_menu_item));
 
   /* Silent return for the constructor */
   if (accel_group) {
 
     GtkStockItem stock_item;
 
-    g_return_if_fail (GEDA_IS_IMAGE_MENU_ITEM (image_menu_item));
     g_return_if_fail (GTK_IS_ACCEL_GROUP (accel_group));
 
     if (image_menu_item->use_stock &&
