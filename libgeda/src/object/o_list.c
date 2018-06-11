fd083bc564044f18df0c5ca30699b0fc207baf88
WileyECoyote <wileyhill@gmail.com>
2018-06-11 00:43:59 -0500

    o_list.c: remove g_return_val_if_fail attribute not text

--- libgeda/src/object/o_list.c
+++ libgeda/src/object/o_list.c
@@ -155,7 +155,6 @@ GedaObject *geda_object_list_find_attrib_by_name (const GList *list,
     GedaObject *attribute = iter->data;
 
     char *found_name;
-    g_return_val_if_fail (attribute->type == OBJ_TEXT, NULL);
 
     if (!geda_attrib_object_get_name_value (attribute, &found_name, NULL))
       continue;
