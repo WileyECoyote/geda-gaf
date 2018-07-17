374ac389011cd9554e247c5e75a374a4b5e12d4f
WileyECoyote <wileyhill@gmail.com>
2018-07-17 11:01:49 -0500

    libgeda: include error strings in message output

--- libgeda/src/file/f_file.c
+++ libgeda/src/file/f_file.c
@@ -410,14 +410,14 @@ void geda_file_remove_backup (const char *filename)
 #if defined (OS_WIN32_NATIVE)
 
         if (chmod(backup_filename, 666)) {
-          fprintf (stderr, "%s: %s %s\n", __func__, backup_filename, strerror (errno));
+          fprintf (stderr, "%s: %s, %s\n", __func__, backup_filename, strerror (errno));
         }
 
 #endif
 
         if (unlink(backup_filename) != 0) {
           const char *log_msg = _("Unable to delete backup file");
-          geda_log ("%s %s.\n", log_msg, filename);
+          geda_log ("%s %s, %s.\n", log_msg, filename, strerror (errno));
         }
       }
 
