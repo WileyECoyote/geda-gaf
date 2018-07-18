54b6fa4eee8711156cea9da8a742536f4c8a0433
WileyECoyote <wileyhill@gmail.com>
2018-07-18 00:43:18 -0500

    t0001-geda-conf-lib.scm: account for build platform in "build-path"

--- libgeda/scheme/unit-tests/t0001-geda-conf-lib.scm
+++ libgeda/scheme/unit-tests/t0001-geda-conf-lib.scm
@@ -1,17 +1,16 @@
 ;; Test Scheme procedures defined in geda.scm.  Makes blatant
 ;; assumptions about the current directory.  Oh well.
 
-(use-modules (unit-test))
+(use-modules (unit-test) (geda os))
 
 (load-from-path "geda.scm")
 
 (begin-test 'build-path
- (assert-equal
-  "prefix/suffix"
-  (build-path "prefix" "suffix"))
- (assert-equal
-  "/path/to/a/directory"
-  (build-path "/path" "to" "a" "directory")))
+ (let* ((path1 (string-append "prefix" separator "suffix"))
+        (path2 (string-append "path" separator "to" separator "a" separator "directory")))
+
+       (assert-equal path1 (build-path "prefix" "suffix"))
+       (assert-equal path2 (build-path "path" "to" "a" "directory"))))
 
 (begin-test 'regular-file?
  (assert-true (regular-file? "Makefile"))
