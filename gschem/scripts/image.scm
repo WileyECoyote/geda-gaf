97d2bdf7b4c1463ecbf635d978dc5e31fe3a55c8
WileyECoyote <wileyhill@gmail.com>
2018-07-19 09:14:28 -0500

    scripts/image.scm: update instructions, add commented "invert-images"

--- gschem/scripts/image.scm
+++ gschem/scripts/image.scm
@@ -1,16 +1,24 @@
 ;; $Id$
 ;;
-;; This file may be used to produce png files from geda schematics from the
-;; command line.  Typical usage is:
+;; This file may be used to produce png files from geda schematics from
+;; the command line. Typical usage is:
 ;;
-;;   gschem -p -o mysch.png -r /path/to/this/file/image.scm mysch.sch
+;;   gschem -z -o mysch.png -r /path/to/this/file/image.scm mysch.sch
 ;;
 ;; The schematic in "mysch.sch" will be exported to the file "mysch.png"
+;;
+;; The image output file extension can be bmp, png, jpeg, jpg, pdf, tif,
+;; or tiff.
+;;
 
 (image-size 1024 768)
 ;(image-size 3200 2400)
+
 (image-color "enabled")
 ;(image-color "disabled")
+
+;(invert-images "enabled")
+;(invert-images "disabled")
 
 (anti-aliasing "subpixel")
 
