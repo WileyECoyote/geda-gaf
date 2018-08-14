621de37b471df5b95433fbd2555f555b7f2c2548
WileyECoyote <wileyhill@gmail.com>
2018-08-14 10:12:36 -0500

    gschem: stop action if add pin not hot

--- gschem/src/iface/i_command.c
+++ gschem/src/iface/i_command.c
@@ -3432,17 +3432,13 @@ COMMAND (do_add_pin)
   o_redraw_cleanstates(w_current);
   o_invalidate_rubber (w_current);
 
-  if (w_current->action_event->state) {
-    i_event_cancel_action_handler(w_current);
-  }
-
   i_status_set_state(w_current, PINMODE);
 
   if HOT_ACTION (do_add_pin) {
     o_pin_start (w_current, CMD_X(do_add_pin), CMD_Y(do_add_pin));
   }
   else {
-    i_status_action_stop(w_current);
+    i_event_end_action_handler(w_current);
   }
 
   EXIT_COMMAND(do_add_pin);
