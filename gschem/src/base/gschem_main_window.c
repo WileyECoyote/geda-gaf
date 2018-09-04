742e0e7aaa5e523c2ff9f2626ad84fd4f8fbfb26
WileyECoyote <wileyhill@gmail.com>
2018-09-04 03:20:42 -0500

    gschem_main_window.c: clear error

--- gschem/src/base/gschem_main_window.c
+++ gschem/src/base/gschem_main_window.c
@@ -509,6 +509,7 @@ gschem_main_window_instance_init (GTypeInstance *instance, void *class)
   }
   else if (verbose_mode) {
     fprintf(stderr, "%s is missing\n", filename);
+    g_error_free(error);
   }
   GEDA_FREE(filename);
 }
