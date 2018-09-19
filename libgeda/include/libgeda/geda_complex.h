0dfdb31e6567e453a4a2349f17f564d4d83595f4
WileyECoyote <wileyhill@gmail.com>
2018-09-19 02:26:35 -0500

    libgeda: wrap is_a_geda_complex parameter in parentheses

--- libgeda/include/libgeda/geda_complex.h
+++ libgeda/include/libgeda/geda_complex.h
@@ -40,7 +40,7 @@
 #define GEDA_TYPE_COMPLEX            (geda_complex_get_type())
 #define GEDA_COMPLEX(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj), GEDA_TYPE_COMPLEX, GedaComplex))
 #define GEDA_COMPLEX_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass),  GEDA_TYPE_COMPLEX, GedaComplexClass))
-#define GEDA_IS_COMPLEX(obj)         (is_a_geda_complex((GedaComplex*)obj))
+#define GEDA_IS_COMPLEX(obj)         (is_a_geda_complex((GedaComplex*)(obj)))
 #define GEDA_IS_COMPLEX_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass),  GEDA_TYPE_COMPLEX))
 #define GEDA_COMPLEX_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj),  GEDA_TYPE_COMPLEX, GedaComplexClass))
 
