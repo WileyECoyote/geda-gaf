1bb9b19ff395702cee4561766dc8712a1da89cb0
WileyECoyote <wileyhill@gmail.com>
2018-10-02 09:49:44 -0500

    gsche: revise add circle action event handling

--- gschem/src/iface/i_command.c
+++ gschem/src/iface/i_command.c
@@ -3482,17 +3482,13 @@ COMMAND (do_add_circle)
   o_redraw_cleanstates(w_current);
   o_invalidate_rubber (w_current);
 
-  if (w_current->action_event->state) {
-    i_event_cancel_action_handler(w_current);
-  }
-
   i_status_set_state(w_current, CIRCLEMODE);
 
   if HOT_ACTION (do_add_circle) {
     o_circle_start (w_current, CMD_X(do_add_circle), CMD_Y(do_add_circle));
   }
   else {
-    i_status_action_stop(w_current);
+    i_event_end_action_handler(w_current);
   }
 
   EXIT_COMMAND(do_add_circle);
