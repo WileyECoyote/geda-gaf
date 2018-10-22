9236e02e758c2f9e674a2d572d19011e6363911e
WileyECoyote <wileyhill@gmail.com>
2018-10-22 06:28:24 -0500

    geda_object.h: declare geda_get_object_type static in header

--- libgeda/include/libgeda/geda_object.h
+++ libgeda/include/libgeda/geda_object.h
@@ -158,7 +158,8 @@ void          geda_object_set_selectable     (GedaObject *object, int state);
 void          geda_object_set_locked_color   (GedaObject *object, int color);
 void          geda_object_set_page           (GedaObject *object, Page *page);
 
-inline int    geda_get_object_type           (GedaObject *object)
+static inline
+int           geda_get_object_type           (GedaObject *object)
 {
     return object->type;
 };
