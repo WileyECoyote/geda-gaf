2650f08bc1a7b2a9cf61599fe150f162e7fbc7cd
WileyECoyote <wileyhill@gmail.com>
2018-10-26 12:51:29 -0500

    travis-after-failure: show examples test-suite.log files

--- travis-after-failure.sh
+++ travis-after-failure.sh
@@ -15,3 +15,15 @@ test -f ./libgedathon/tests/run_tests.sh.log && cat ./libgedathon/tests/run_test
 test -f ./libgedauio/tests/test-suite.log && cat ./libgedauio/tests/test-suite.log
 
 test -f ./gschem/tests/test-suite.log && cat ./gschem/tests/test-suite.log
+
+# Examples
+
+test -f ./examples/CascodeAmp/test-suite.log && cat ./examples/CascodeAmp/test-suite.log
+
+test -f ./examples/gTAG/test-suite.log && cat ./examples/gTAG/test-suite.log
+
+test -f ./examples/lightning_detector/test-suite.log && cat ./examples/lightning_detector/test-suite.log
+
+test -f ./examples/MSA-2643/test-suite.log && cat ./examples/MSA-2643/test-suite.log
+
+test -f ./examples/TwoStageAmp/test-suite.log && cat ./examples/TwoStageAmp/test-suite.log
