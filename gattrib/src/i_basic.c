e04eaaf29ff63d6a0c071409e5b6501b06e46cb5
WileyECoyote <wileyhill@gmail.com>
2019-02-11 10:18:54 -0600

    gattrib: remove unused function i_update_status

--- gattrib/src/i_basic.c
+++ gattrib/src/i_basic.c
@@ -31,26 +31,7 @@
 
 #include <geda_debug.h>
 
-/* ------------------------------------------------------------- *
- *
- * ------------------------------------------------------------- */
-#if 0 /* not used, but leaving it here in case we need it later */
-static void i_update_status(GedaToplevel *toplevel, const char *string)
-{
-  if (!toplevel->status_label) {
-    return;
-  }
 
-  if (string) {
-    /* NOTE: consider optimizing this if same label */
-    gtk_label_set(GTK_LABEL(toplevel->status_label), (char *) string);
-  }
-}
-#endif
-
-/* ------------------------------------------------------------- *
- *
- * ------------------------------------------------------------- */
 #ifdef OS_WIN32
 #  ifndef STRICT
 #    define STRICT
