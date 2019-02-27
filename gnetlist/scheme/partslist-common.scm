92e26539e2a548e7b9b8449039bd8917ea7c8fd7
WileyECoyote <wileyhill@gmail.com>
2019-02-27 12:20:46 -0600

    getlist: exclude directives and models from part lists

--- gnetlist/scheme/partslist-common.scm
+++ gnetlist/scheme/partslist-common.scm
@@ -25,7 +25,9 @@
   (if (null? packages)
      '()
       (let ((package (car packages)))
-        (if (or (string=? (get-device package) "include")
+        (if (or (string=? (get-device package) "directive")
+                (string=? (get-device package) "include")
+                (string=? (get-device package) "model")
                 (string=? "1" (get-package-attribute package "nobom")))
             (get-parts-table (cdr packages))
             (cons (list package
