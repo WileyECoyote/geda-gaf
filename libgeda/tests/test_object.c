8c791cf7a36c89ca39976041d1f5072a3e5a82db
WileyECoyote <wileyhill@gmail.com>
2019-03-21 11:03:16 -0500

    libgeda/tests: check geda_object_bounds with GedaBus

--- libgeda/tests/test_object.c
+++ libgeda/tests/test_object.c
@@ -721,6 +721,42 @@ int check_methods (void)
   geda_object_unref(object);
 
   /* ------------------------- GedaBus -------------------------- */
+
+  object = geda_bus_object_new (3, 100, 100, 200, 200, 0);
+
+  if (!geda_object_bounds(object)) {
+    fprintf(stderr, "Failed: bus object_bounds %s line <%d>\n", TOBJECT, __LINE__);
+    result++;
+  }
+  else {
+
+    if (!geda_object_get_bounds_valid(object)) {
+      fprintf(stderr, "Failed: get_bounds_valid %s line <%d>\n", TOBJECT, __LINE__);
+      result++;
+    }
+
+    if (object->left != 100) {
+      fprintf(stderr, "Failed: bus object_bounds %s; left <%d>\n", TOBJECT, object->left);
+      result++;
+    }
+
+    if (object->right != 200) {
+      fprintf(stderr, "Failed: bus object_bounds %s; right <%d>\n", TOBJECT, object->right);
+      result++;
+    }
+
+    if (object->top != 100) {
+      fprintf(stderr, "Failed: bus object_bounds %s; top <%d>\n", TOBJECT, object->top);
+      result++;
+    }
+
+    if (object->bottom != 200) {
+      fprintf(stderr, "Failed: bus object_bounds %s; bottom <%d>\n", TOBJECT, object->bottom);
+      result++;
+    }
+  }
+  geda_object_unref(object);
+
   /* ------------------------- GedaCircle ----------------------- */
   /* ------------------------- GedaComplex ---------------------- */
   /* ------------------------- GedaLine ------------------------- */
