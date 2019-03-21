750d6fde6cb9c907334576e1367b6b69d2a8bb7e
WileyECoyote <wileyhill@gmail.com>
2019-03-21 10:57:01 -0500

    libgeda: remove commented cruft

--- libgeda/src/file/f_sys.c
+++ libgeda/src/file/f_sys.c
@@ -479,7 +479,6 @@ char *geda_file_sys_normalize_name (const char *name, GError **error)
     /* The file system is case-preserving but case-insensitive,
      * canonicalize to lowercase, using the codepage associated
      * with the process locale.  */
-    //CharLowerBuff (buf, len);
     result = g_strdup (buf);
   }
 
