3b6062ace7d3eb6ce5622072a678bfca6fe4c7b7
WileyECoyote <wileyhill@gmail.com>
2019-05-06 10:28:05 -0500

    edacairo.h: reorder functions mostly alphabetically

--- libgedacairo/include/edacairo.h
+++ libgedacairo/include/edacairo.h
@@ -34,10 +34,12 @@ typedef enum {
 extern "C" {        /* c++ do not mangle function names */
 #endif
 
-void eda_cairo_set_source_color (cairo_t *cr, int color, GArray *map);
+void eda_cairo_arc (cairo_t *cr, int flags, double width, double x, double y,
+                    double radius, double start_angle, double arc_sweep);
 
-void eda_cairo_line (cairo_t *cr, int flags, int line_end, double w_line_width,
-                     double w_x1, double w_y1, double w_x2, double w_y2);
+void eda_cairo_center_arc (cairo_t *cr, int flags, double center_width,
+                           double line_width, double x, double y,
+                           double radius, double start_angle, double arc_sweep);
 
 void eda_cairo_box (cairo_t *cr, int flags, double line_width,
                     double x1, double y1, double x2, double y2);
@@ -46,18 +48,16 @@ void eda_cairo_center_box (cairo_t *cr, int flags, double center_width,
                            double line_width, double x, double y,
                            double half_width, double half_height);
 
-void eda_cairo_arc (cairo_t *cr, int flags, double width, double x, double y,
-                    double radius, double start_angle, double arc_sweep);
-
-void eda_cairo_center_arc (cairo_t *cr, int flags, double center_width,
-                           double line_width, double x, double y,
-                           double radius, double start_angle, double arc_sweep);
-
-void eda_cairo_stroke (cairo_t *cr, int flags, int line_type, int line_end,
-                       double wwidth, double wlength, double wspace);
+void eda_cairo_line (cairo_t *cr, int flags, int line_end, double w_line_width,
+                     double w_x1, double w_y1, double w_x2, double w_y2);
 
 void eda_cairo_path (cairo_t *cr, int flags, double line_width, int nsections,
                      PATH_SECTION *sections);
+
+void eda_cairo_set_source_color (cairo_t *cr, int color, GArray *map);
+
+void eda_cairo_stroke (cairo_t *cr, int flags, int line_type, int line_end,
+                       double wwidth, double wlength, double wspace);
 
 #ifdef __cplusplus
 }
