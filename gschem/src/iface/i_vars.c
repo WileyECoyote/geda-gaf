3ba586d46132da082b12c3aa5213c619a6ff7fdc
WileyECoyote <wileyhill@gmail.com>
2019-06-20 09:55:28 -0500

    gschem: add function documentation

--- gschem/src/iface/i_vars.c
+++ gschem/src/iface/i_vars.c
@@ -311,7 +311,19 @@ bool i_var_restore_group_double (EdaConfig *cfg, const char *group, const char *
   return result;
 }
 
-/* Retrieves integer from key file in specified group */
+/*!
+ * \brief Retrieve a integer from key file in and specified group
+ * \par Function Description
+ *  Retrieves a integer from key file in specified group. Returns True
+ *  if the value was restored from configuration or False if \a def_val
+ *  was assigned.
+ *
+ * \param cfg      Pointer to EdaConfig Object
+ * \param group    String name of configuration group
+ * \param key      string name of record key to be retrieved
+ * \param var      Pointer to variable whose value is to be set
+ * \param def_val  default returned value if key does not exist.
+ */
 bool i_var_restore_group_integer(EdaConfig *cfg, const char *group, const char *key, int *var, int def_val)
 {
   GError *err = NULL;
