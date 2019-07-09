2cb2e3ffc39f92dc67f17fd697273eb7ed69c5da
WileyECoyote <wileyhill@gmail.com>
2019-07-09 10:49:56 -0500

    libgeda: move checks to t0002-g_rc.scm

--- libgeda/scheme/unit-tests/t0002-g_rc.scm
+++ libgeda/scheme/unit-tests/t0002-g_rc.scm
@@ -1,5 +1,17 @@
 ;; Test routines defined in src/guile/g_rc.c.
 
+(begin-test 'attribute-promotion/getter
+  (let ((mode (attribute-promotion)))
+    (assert-equal #t mode)))
+
+(begin-test 'promote-invisible/getter
+  (let ((mode (promote-invisible)))
+    (assert-equal #f mode)))
+
+(begin-test 'keep-invisible/getter
+  (let ((mode (keep-invisible)))
+    (assert-equal #t mode)))
+
 ;; Check bitmap-directory returns value
 (begin-test 'bitmap-directory/getter
   (let ((path (bitmap-directory)))
--- libgeda/scheme/unit-tests/t0301-promotable-attributes.scm
+++ libgeda/scheme/unit-tests/t0301-promotable-attributes.scm
@@ -29,14 +29,4 @@
     (page-append! P p)
     (assert-equal '() (promote-attribs! p))))
 
-(begin-test 'attribute-promotion/getter
-  (let ((mode (attribute-promotion)))
-    (assert-equal #t mode)))
-
-(begin-test 'promote-invisible/getter
-  (let ((mode (promote-invisible)))
-    (assert-equal #f mode)))
-
-(begin-test 'keep-invisible/getter
-  (let ((mode (keep-invisible)))
-    (assert-equal #t mode)))
+;; See t0002-g_rc.scm
