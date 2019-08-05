3029367a6b9f9404f73a8f2d536b499ae0bf9bbd
WileyECoyote <wileyhill@gmail.com>
2019-08-05 12:02:52 -0500

    gschem: Fix module name in comments

--- gschem/src/guile/g_attrib.c
+++ gschem/src/guile/g_attrib.c
@@ -146,10 +146,10 @@ SCM_DEFINE (add_attrib_x, "%add-attrib!", 6, 0, 0,
 }
 
 /*!
- * \brief Create the (gschem core object) Scheme module.
+ * \brief Create the (gschem core attrib) Scheme module.
  * \par Function Description
- * Defines procedures in the (gschem core object) module. The module can
- * be accessed using (use-modules (gschem core object)).
+ * Defines procedures in the (gschem core attrib) module. The module can
+ * be accessed using (use-modules (gschem core attrib)).
  */
 static void
 init_module_gschem_core_attrib ()
