2f49690494d77b603fc1c97d650686d6ecb7ac2b
WileyECoyote <wileyhill@gmail.com>
2019-09-25 10:22:23 -0500

    geda_combobox.c: update parameter documentation reference

--- libgedauio/src/widgets/geda_combobox.c
+++ libgedauio/src/widgets/geda_combobox.c
@@ -5886,7 +5886,7 @@ GtkWidget *geda_combo_box_new_text_with_entry (void)
 /*!
  * \brief Append Text to a GedaComboBox
  * \par Function Description
- *  Appends \a string to the list of strings stored in \a combo_box. Note
+ *  Appends \a text to the list of strings stored in \a combo_box. Note
  *  that this can only use this function with combo boxes constructed with
  *  geda_combo_box_new_text().
  *
