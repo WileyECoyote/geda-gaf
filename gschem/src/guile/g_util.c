5895a2598d0e62c88a351b3d772bbb606dcd20d9
WileyECoyote <wileyhill@gmail.com>
2019-10-03 12:43:27 -0500

    gschem: separate declaration

--- gschem/src/guile/g_util.c
+++ gschem/src/guile/g_util.c
@@ -59,13 +59,15 @@
 SCM_DEFINE (show_uri, "%show-uri", 1, 0, 0, (SCM uri_s),
             "Show a URI in the associated default application")
 {
+  const char *uri;
+
   /* Check that we were passed a string. */
   SCM_ASSERT (scm_is_string (uri_s), uri_s, SCM_ARG1, s_show_uri);
 
-  const char *uri = scm_to_utf8_string (uri_s);
+  uri = scm_to_utf8_string (uri_s);
 
   if (!x_show_uri (uri)) {
-    u_log_message( "%s %s\n", _("Could not launch URI"), uri);
+    u_log_message( "%s <%s>\n", _("Could not launch URI"), uri);
   }
 
   return SCM_UNDEFINED;
