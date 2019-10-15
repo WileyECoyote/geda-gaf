0591a8de02727d0048c8b1fdcea442155067800c
WileyECoyote <wileyhill@gmail.com>
2019-10-15 00:38:57 -0500

    geda_container.c: replace GINT_TO_POINTER with INT_TO_POINTER

--- libgedauio/src/functions/geda_container.c
+++ libgedauio/src/functions/geda_container.c
@@ -409,7 +409,7 @@ geda_container_focus_sort_tab (GtkContainer     *container,
 {
   GtkTextDirection text_direction = gtk_widget_get_direction (GTK_WIDGET (container));
 
-  children = g_list_sort_with_data (children, tab_compare, GINT_TO_POINTER (text_direction));
+  children = g_list_sort_with_data (children, tab_compare, INT_TO_POINTER (text_direction));
 
   /* if we are going backwards then reverse the order
    *  of the children.
