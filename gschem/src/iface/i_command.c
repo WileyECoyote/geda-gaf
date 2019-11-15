b5bde6370dc1dad0c5cecf5db6c1cc4e59d1748b
WileyECoyote <wileyhill@gmail.com>
2019-11-15 10:15:02 -0600

    i_command.c: fix capitalization in documentation

--- gschem/src/iface/i_command.c
+++ gschem/src/iface/i_command.c
@@ -830,7 +830,7 @@ COMMAND (do_file_new_window)
  *  @{\par This group contains functions to open documents
  *    \ingroup i_command_File_Actions
  */
-/* This does not do anything productive, is a delay, the destroy
+/* This does not do anything productive, is a delay. The destroy
  * notifier; open_command_idle_notify, does all the work. This is
  * a low priority main-loop task, instigated after higher priority
  * main-loop task were delegated to opening files. If the loader is
