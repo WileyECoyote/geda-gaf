2bfced26e3d0b48cd13218573244577dcaf4d995
WileyECoyote <wileyhill@gmail.com>
2019-11-29 21:54:46 -0600

    libgeda/tests: add skelton module to check m_math.c

---
+++ libgeda/tests/test_math.c
@@ -0,0 +1,86 @@
+
+/* -*- test_math.c -*-
+ *
+ * gEDA - GPL Electronic Design Automation
+ * libgeda - gEDA's library
+ *
+ * Copyright (C) 2016 gEDA Contributors (see ChangeLog for details)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA, <http://www.gnu.org/licenses/>.
+ *
+ *  Contributing Author: Wiley Edward Hill
+ *  Date Contributed: November, 29nd, 2019
+ */
+
+#include "../../config.h"
+
+#include <libgeda.h>
+#include <math.h>
+
+#include "test-suite.h"
+
+/*! \def MUT Module Under Tests */
+#define MUT "src/math/m_math.c"
+
+/*! \file test_math.c
+ *  \brief Tests for geda math functions
+ *  \par
+ *   This module includes tests for functions in src/math.c.
+ *
+ *  Test Identifiers:  M  01  88  88
+ *                     ^   ^   ^   ^
+ *    group-code ______|   |   |   |
+ *                         |   |   |
+ *    Module/File No. _____|   |   |
+ *                             |   |
+ *    Function Number _________|   |
+ *                                 |
+ *    Tests Number ________________|
+ *
+ *  See tests/README for more details on the nomenclature for test identifiers.
+ *
+ *      M0101    geda_math_degrees_to_radians
+ *      M0102    geda_math_distance
+ *      M0103    geda_math_papersize_to_world
+ *      M0104    geda_math_random_number
+ *      M0105    geda_math_radians_to_degrees
+ *      M0106    geda_math_rotate_point
+ *      M0107    geda_math_rotate_point_90
+ */
+
+int main (int argc, char *argv[])
+{
+  int result = 0;
+
+  SETUP_SIGSEGV_HANDLER;
+
+  /* Initialize gobject */
+#if (( GLIB_MAJOR_VERSION == 2 ) && ( GLIB_MINOR_VERSION < 36 ))
+  g_type_init();
+#endif
+
+/*
+  if (setjmp(point) == 0) {
+    result += geda_math_degrees_to_radians();
+  }
+  else {
+    fprintf(stderr, "Caught signal checking degrees_to_radians in %s\n\n", MUT);
+    return 1;
+  }
+*/
+
+  return result;
+}
