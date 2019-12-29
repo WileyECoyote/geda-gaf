d744181355219afa601be6358db3870867c25818
WileyECoyote <wileyhill@gmail.com>
2019-12-28 21:03:12 -0600

    i_vars.c: remove empty documentation line

--- gnetlist/src/i_vars.c
+++ gnetlist/src/i_vars.c
@@ -22,7 +22,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301 USA, <http://www.gnu.org/licenses/>.
- *
  */
 
 #include "../../config.h"
