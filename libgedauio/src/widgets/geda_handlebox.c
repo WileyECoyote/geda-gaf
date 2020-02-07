157aa9a9ded066d10073c8c7244c6ede312fd64c
WileyECoyote <wileyhill@gmail.com>
2020-02-07 13:09:22 -0600

    geda_handlebox.c: utilize local reference to GtkStyle

--- libgedauio/src/widgets/geda_handlebox.c
+++ libgedauio/src/widgets/geda_handlebox.c
@@ -1220,7 +1220,7 @@ static void geda_handle_box_realize (GtkWidget *widget)
 
   state = gtk_widget_get_state (widget);
 
-  gtk_style_set_background (widget->style, widget->window, state);
+  gtk_style_set_background (style, widget->window, state);
   gtk_style_set_background (widget->style, handlebox->bin_window, state);
   gtk_style_set_background (widget->style, handlebox->float_window, state);
 
