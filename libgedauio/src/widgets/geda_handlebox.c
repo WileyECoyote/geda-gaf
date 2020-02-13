ed8a1aade9f06714e562235589d854637ff07fe6
WileyECoyote <wileyhill@gmail.com>
2020-02-13 11:55:15 -0600

    geda_handlebox.c: utilize local reference to window

--- libgedauio/src/widgets/geda_handlebox.c
+++ libgedauio/src/widgets/geda_handlebox.c
@@ -1223,7 +1223,7 @@ static void geda_handle_box_realize (GtkWidget *widget)
   gtk_style_set_background (style, handlebox->bin_window, state);
   gtk_style_set_background (style, handlebox->float_window, state);
 
-  gdk_window_set_back_pixmap (widget->window, NULL, TRUE);
+  gdk_window_set_back_pixmap (window, NULL, TRUE);
 
   connect_settings_signal(handlebox);
 }
