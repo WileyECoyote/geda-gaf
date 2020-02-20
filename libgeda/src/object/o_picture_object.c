f4e7d75e860dc87fa46feffd5d41a71f53450368
WileyECoyote <wileyhill@gmail.com>
2020-02-20 11:29:03 -0600

    libgeda: add function geda_picture_object_get_lower_y

--- libgeda/src/object/o_picture_object.c
+++ libgeda/src/object/o_picture_object.c
@@ -647,6 +647,20 @@ int geda_picture_object_get_lower_x (const GedaObject *object)
 
   return object->picture->lower_x;
 }
+/*!
+ * \brief Get Width of a Picture
+ * \par Function Description
+ *  This function returns current height if picture \a object
+ *  in world units. No checking is performed.
+ *
+ * \param [in] object   The object to get the position.
+ */
+int geda_picture_object_get_lower_y (const GedaObject *object)
+{
+  g_return_val_if_fail (GEDA_IS_PICTURE(object), -0);
+
+  return object->picture->lower_y;
+}
 
 /*!
  * \brief Get mask data from image
