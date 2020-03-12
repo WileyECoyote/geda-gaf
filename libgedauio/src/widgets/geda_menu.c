ff57ae4fd4a9d3fa4b95010c4b267b388381474c
WileyECoyote <wileyhill@gmail.com>
2020-03-12 15:00:29 -0500

    geda_menu.c: utilize assigned reference to attributes.width

--- libgedauio/src/widgets/geda_menu.c
+++ libgedauio/src/widgets/geda_menu.c
@@ -1917,8 +1917,8 @@ static void geda_menu_realize (GtkWidget *widget)
 
   attributes.x = border_width + style->xthickness + horizontal_padding;
   attributes.y = border_width + style->ythickness + vertical_padding;
-  attributes.width  = MAX (1, widget->allocation.width - attributes.x * 2);
   attributes.height = MAX (1, widget->allocation.height - attributes.y * 2);
+  attributes.width  = MAX (1, attributes.width - attributes.x * 2);
 
   menu = (GedaMenu*)widget;
 
