a4a3d4968dc83f069e9abc34bfae23ee9ea39c9a
WileyECoyote <wileyhill@gmail.com>
2020-03-19 12:00:51 -0500

    geda_gtk_compat.h: fix geda_get_adjustment_page_size Gtk > 2

--- libgedauio/include/geda_gtk_compat.h
+++ libgedauio/include/geda_gtk_compat.h
@@ -254,7 +254,7 @@ void gdk_font_unref (GdkFont *font);
 
 #define geda_get_adjustment_lower(a) gtk_adjustment_get_lower((GtkAdjustment*)a)
 
-#define geda_get_adjustment_page_size(a) gtk_adjustment_get_lower((GtkAdjustment*)a)
+#define geda_get_adjustment_page_size(a) gtk_adjustment_get_page_size((GtkAdjustment*)a)
 
 #define geda_get_adjustment_upper(a) gtk_adjustment_get_lower((GtkAdjustment*)a)
 
