66408b9d6de9799b4f6e0a6b48411b22e760ff16
WileyECoyote <wileyhill@gmail.com>
2020-04-21 13:38:25 -0500

    geda_gtk_compat.h: add macros geda_get_button_use_stock

--- libgedauio/include/geda_gtk_compat.h
+++ libgedauio/include/geda_gtk_compat.h
@@ -194,6 +194,8 @@ gtk_window_group_get_current_grab (GtkWindowGroup *window_group)
 
 #define geda_get_button_label_text(b) ((GtkButton*)b)->label_text
 
+#define geda_get_button_use_stock(b) ((GtkButton*)b)->use_stock
+
 /*! \def geda_get_child_widget Get Child Bin widget Gtk < 3 */
 #define geda_get_child_widget(w) (void*)((GtkBin*)w)->child
 
@@ -283,6 +285,8 @@ void gdk_font_unref (GdkFont *font);
 #define geda_get_button_focus_on_click(b) gtk_button_get_focus_on_click((GtkButton*)b)
 
 #define geda_get_button_label_text(b) gtk_button_get_label((GtkButton*)b)
+
+#define geda_get_button_use_stock(b) gtk_button_get_use_stock((GtkButton*)b)
 
 /*! \def geda_get_child_widget Get Child Bin widget Gtk >= 3*/
 #define geda_get_child_widget(w) (void*)gtk_bin_get_child ((GtkBin*)w)
