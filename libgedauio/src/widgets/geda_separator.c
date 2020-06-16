7a7e1d941714b57c524de0c6317392efc2880217
WileyECoyote <wileyhill@gmail.com>
2020-06-16 12:50:48 -0500

    geda_separator.c: utilize local reference to widget window

--- libgedauio/src/widgets/geda_separator.c
+++ libgedauio/src/widgets/geda_separator.c
@@ -121,8 +121,7 @@ geda_separator_expose (GtkWidget *widget, GdkEventExpose *event)
   else {
 
     if (wide_separators) {
-      gtk_paint_box (style,
-                     widget->window, state, GTK_SHADOW_ETCHED_OUT,
+      gtk_paint_box (style, window, state, GTK_SHADOW_ETCHED_OUT,
                      &event->area, widget, "vseparator",
                      widget->allocation.x +
                     (widget->allocation.width - separator_width) / 2,
