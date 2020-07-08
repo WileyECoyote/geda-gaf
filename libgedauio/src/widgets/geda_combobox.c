ed45a2a09ad29f5300bd9793cbe709e14f00373b
WileyECoyote <wileyhill@gmail.com>
2020-07-08 11:41:33 -0500

    geda_combobox.c: replace uneeded GTK_LIST_STORE dynamic cast

--- libgedauio/src/widgets/geda_combobox.c
+++ libgedauio/src/widgets/geda_combobox.c
@@ -6107,7 +6107,7 @@ void geda_combo_box_insert_text (GedaComboBox *combo_box,
 
     g_return_if_fail (g_utf8_validate(text, -1, NULL));
 
-    store = GTK_LIST_STORE (combo_box->priv->model);
+    store = (GtkListStore*)combo_box->priv->model;
 
     if (position >= 0) {
       gtk_list_store_insert (store, &iter, position);
