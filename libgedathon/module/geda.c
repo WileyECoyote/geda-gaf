9af950e626d96c019b0f6c340ab6a2856820d2df
WileyECoyote <wileyhill@gmail.com>
2020-08-03 12:56:50 -0500

    libgedathon: revise documentation; fix grammar

--- libgedathon/module/geda.c
+++ libgedathon/module/geda.c
@@ -1271,9 +1271,9 @@ METHOD(get_bounds)
 /*!
  * \brief Get an Object from GedaCapsuleObject
  * \par Method Description
- *  This function provides a method to create PyPyGedaObjects from a GedaCapsule
- *  object but is not normally called directly. This method is used by other methods
- *  to get an Python version of the object contained within a Geda capsule.
+ *  This function provides a method to create PyGedaObjects from a GedaCapsule
+ *  object but is not normally called directly. This method is used by other
+ *  methods to get a Python version of the object contained within the capsule.
  *
  *  [in] PyObject capsule  The container object
  *
