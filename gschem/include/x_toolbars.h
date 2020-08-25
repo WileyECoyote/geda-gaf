a141e9891cede6c55dde7e1aa6c11e4309baf536
WileyECoyote <wileyhill@gmail.com>
2020-08-25 11:08:21 -0500

    x_toolbars.h: revise toolbar button tip

--- gschem/include/x_toolbars.h
+++ gschem/include/x_toolbars.h
@@ -120,7 +120,7 @@ typedef struct {
    /* Standard Toolbar*/
 #define TBTS_FILE_NEW           "Create a new file"
 #define TBTS_FILE_OPEN          "Open an existing schematic or symbol file"
-#define TBTS_FILE_SAVE          "Save file"
+#define TBTS_FILE_SAVE          "Save the current document"
 #define TBTS_FILE_SAVE_AS       "Save the file to different name or location"
 #define TBTS_FILE_CLOSE         "Close the current file"
 
