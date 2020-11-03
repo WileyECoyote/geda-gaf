78cc81502b51e651c48e99871b660aade00a80da
WileyECoyote <wileyhill@gmail.com>
2020-11-03 11:42:11 -0600

    gtksheettypebuiltins: remove references to generated data, add header

--- gattrib/src/gtksheet/gtksheettypebuiltins.c
+++ gattrib/src/gtksheet/gtksheettypebuiltins.c
@@ -1,5 +1,20 @@
-
-/* Generated data (by glib-mkenums) */
+/* gtksheettypebuiltins.c
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
 
 #define GTKSHEET_ENABLE_BROKEN
 
@@ -85,6 +100,3 @@ gtk_sheet_vertical_justification_get_type (void)
   }
   return etype;
 }
-
-/* Generated data ends here */
-
